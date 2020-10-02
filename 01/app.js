3ec05c658906a830aa8b2216c9c5cb920e4715e7
Dawid <dwilk92@o2.pl>
2020-10-02 13:31:54 +0200

    zad1 problemy

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,43 @@
 document.addEventListener('DOMContentLoaded', init);
 
-function init() {
-    const divList = document.querySelectorAll('div');
-    
+ function init() {
+   const divList = document.querySelectorAll('div');
+
     setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
+            setBorderColorAsync(divList[1], 'blue', function() {
+                setBorderColorAsync(divList[2], 'green', function() {
+                    console.log('finish');
+                });
             });
         });
-    });
 
-}
+        
+    // setBorderColorAsync(divList[0], 'red', function() {
+    //     setBorderColorAsync(divList[1], 'blue', function() {
+    //         setBorderColorAsync(divList[2], 'green', function() {
+    //             console.log('finish');
+    //         });
+    //     });
+    // });
 
-function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
-}
+ }
+
+const setBorderColorAsync = (element, color, callback) => {
+    const promise = new Promise((resolve, reject) => {
+        const time = Math.random() * 3000;
+        setTimeout(() => {
+            element.style.border = `3px solid ${color}`;
+            callback();
+        }, time);
+        });
+        return promise;
+    }
+//     setBorderColorAsync(divList, color, callback)
+//  .then(resp => console.log(resp))
+//  .catch(err => console.error(err));
+//     setTimeout(() => {
+//         element.style.border = `3px solid ${color}`;
+//         callback();
+//     }, Math.random() * 3000);
+// }
+
