8a3f1552a81cecfce29410281ebc41dc50433919
Dawid <dwilk92@o2.pl>
2020-10-05 20:32:52 +0200

    do 3

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+    
     console.log('DOM');
+    const button = document.querySelector('button');
+    const span = document.querySelector('span');
+    function buttonClick(){
+       // console.log('click')
+        const promise = fetch('https://api.ipify.org?format=json')
+        promise
+            .then(resp => resp.json())
+            .then(ip => span.innerText=ip.ip)
+        //span.innerText= ip;
+    }
+    button.addEventListener('click', buttonClick)
 }
+
+// {
+//     "kind": "Status",
+//     "apiVersion": "v1",
+//     "metadata": {
+      
+//     },
+//     "status": "Failure",
+//     "message": "forbidden: User \"system:anonymous\" cannot get path \"/\"",
+//     "reason": "Forbidden",
+//     "details": {
+      
+//     },
+//     "code": 403
+//   }
--- 04/app.js
+++ 04/app.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+    const lastDiv = document.querySelector('form').lastElementChild
+   // console.log(formEl)
+    const input = lastDiv.querySelector('input')
+    input.addEventListener('click', function(e) {
+        e.preventDefault();
+        // zatrzymujemy domyślną akcję formularza
+        console.log('cl')
+       });
+       
 }
+
+
+// 0fe94766122940729ddf18c937fb3fc2
