ccac96fb4147f64a50a3f6fa332c3d1893e66801
Dawid <dwilk92@o2.pl>
2020-10-07 14:27:48 +0200

    2poprawione

--- 02/app.js
+++ 02/app.js
@@ -1,4 +1,4 @@
-// document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init);
 
 // function init() {
 //     const divList = document.querySelectorAll('div');
@@ -12,44 +12,63 @@
 //     });
 
 // }
-const promise = new Promise((resolve, reject) =>{
-    document.addEventListener('DOMContentLoaded', init);
+//const promise = new Promise((resolve, reject) =>{
 
 function init() {
     const divList = document.querySelectorAll('div');
     
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+    // setBorderColorAsync(divList[0], 'red', function() {
+    //     setBorderColorAsync(divList[1], 'blue', function() {
+    //         setBorderColorAsync(divList[2], 'green', function() {
+    //             console.log('finish');
+    //         });
+    //     });
+    // });
+    // const promise = setBorderColorAsync(divList[0], 'red', firstCallback)
+    
+     function firstCallback() {
+        setBorderColorAsync(divList[1], 'blue', secondCallback);
+        }
+       function secondCallback() {
+        setBorderColorAsync(divList[2], 'green', thirdCallback);
+       }
+       function thirdCallback() {
+        console.log('done?');
+       }
 
+    //    promise.then(resp => console.log(resp))
+    // .catch(err => console.error(err));
+
+    Promise.all([
+        setBorderColorAsync(divList[0], 'red', firstCallback),
+        setBorderColorAsync(divList[1], 'blue', secondCallback),
+        setBorderColorAsync(divList[2], 'green', thirdCallback)
+
+    ])
+    .then(values => console.log(values))
+    .catch(err => console.error(err));
+   
 }
 function setBorderColorAsync(element, color, callback) {
-    
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        
-        if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                resolve('jest')
-                callback();
-            }, Math.random() * 3000);
+    const promise = new Promise((resolve, reject) =>{
+        if(element && element instanceof HTMLElement) {
+            // sprawdzam czy parametr jest elementem DOM, więcej:
+            // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+            
+            if(callback && typeof callback === 'function') {
+                setTimeout(() => {
+                    element.style.border = `3px solid ${color}`;
+                    resolve('jest')
+                    callback();
+                }, Math.random() * 3000);
+            } else {
+                //alert
+                reject('Parametr ~callback~ mus być funkcją');
+            }
         } else {
             //alert
-            reject('Parametr ~callback~ mus być funkcją');
+            reject('Paremetr ~element~ musi być prawidłowym elementem DOM');
         }
-    } else {
-        //alert
-        reject('Paremetr ~element~ musi być prawidłowym elementem DOM');
-    }
-  
-
-}})
-promise
-.then(resp => console.log(resp))
-.catch(err => console.error(err));
+    })
+    return promise
+}
