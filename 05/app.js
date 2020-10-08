120be5fb54f5ee2402beaf7e72eb6b84c8adb629
Dawid <dwilk92@o2.pl>
2020-10-09 01:35:10 +0200

    zad 5 gotowe

--- 05/app.js
+++ 05/app.js
@@ -2,10 +2,34 @@ const apiUrl = 'http://localhost:3000/users';
 
 document.addEventListener('DOMContentLoaded', init);
 
+//console.log(firstName)
 function init() {
     loadUsers();
+    getUsersValue();
 }
 
+function getUsersValue(){
+    const form = document.querySelector('form');
+    form.addEventListener('submit', onClick);
+    }
+
+
+function onClick (event) {
+    event.preventDefault();
+    const firstName = document.querySelector('.form__field--first-name');
+    const lastName = document.querySelector('.form__field--last-name')
+    //console.log(firstName.value, lastName.value)
+    const firstNameVal = firstName.value;
+    const lastNameVal = lastName.value;
+
+    const newUsers = ({
+        firstName: firstNameVal,
+        lastName: lastNameVal
+    })
+    fetchPost(apiUrl, newUsers)
+        .then(() => console.log('ok'))
+        .then(() => alert('błąd'))
+}
 function loadUsers() {
     const promise = fetchGet(apiUrl);
 
@@ -23,6 +47,16 @@ function fetchGet(url) {
 
             return Promise.reject(resp);
         });
+}
+
+function fetchPost(url, data){
+   return fetch(url, {
+
+        method: 'POST',
+        body: JSON.stringify (data),
+        
+        headers: {'Content-Type': 'application/json'}
+    })
 }
 
 function insertUsers(usersList) {
