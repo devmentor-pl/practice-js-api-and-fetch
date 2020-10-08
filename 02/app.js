4c13a551c52cf59e5138576fcbb628d1c0fbf405
Dawid <dwilk92@o2.pl>
2020-10-09 01:32:10 +0200

    zad 2 ostatnia poprawka

--- 02/app.js
+++ 02/app.js
@@ -26,25 +26,24 @@ function init() {
     // });
     // const promise = setBorderColorAsync(divList[0], 'red', firstCallback)
     
-     function firstCallback() {
-        setBorderColorAsync(divList[1], 'blue', secondCallback);
-        }
-       function secondCallback() {
-        setBorderColorAsync(divList[2], 'green', thirdCallback);
-       }
-       function thirdCallback() {
-        console.log('done?');
-       }
+    //  function firstCallback() {
+    //     setBorderColorAsync(divList[1], 'blue', secondCallback);
+    //     }
+    //    function secondCallback() {
+    //     setBorderColorAsync(divList[2], 'green', thirdCallback);
+    //    }
+    //    function thirdCallback() {
+    //     console.log('done?');
+    //    }
 
     //    promise.then(resp => console.log(resp))
     // .catch(err => console.error(err));
 
-    Promise.all([
-        setBorderColorAsync(divList[0], 'red', firstCallback),
-        setBorderColorAsync(divList[1], 'blue', secondCallback),
-        setBorderColorAsync(divList[2], 'green', thirdCallback)
+        setBorderColorAsync(divList[0], 'red')
+            .then(() => setBorderColorAsync(divList[1], 'blue'))
+            .then(()=>setBorderColorAsync(divList[2], 'green'))
+        
 
-    ])
     .then(values => console.log(values))
     .catch(err => console.error(err));
    
@@ -54,17 +53,13 @@ function setBorderColorAsync(element, color, callback) {
         if(element && element instanceof HTMLElement) {
             // sprawdzam czy parametr jest elementem DOM, więcej:
             // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-            
-            if(callback && typeof callback === 'function') {
+           
                 setTimeout(() => {
                     element.style.border = `3px solid ${color}`;
                     resolve('jest')
                     callback();
                 }, Math.random() * 3000);
-            } else {
-                //alert
-                reject('Parametr ~callback~ mus być funkcją');
-            }
+
         } else {
             //alert
             reject('Paremetr ~element~ musi być prawidłowym elementem DOM');
