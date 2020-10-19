53454ba13df6333c2dec4e049ec3efc171e9d0d7
Martyna Kiljan <kmartynak@icloud.com>
2020-10-19 14:03:19 +0200

    ..

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', init);
+const btn = document.querySelector('button');
+// console.log(spanIP)
+// console.log(btn)
 
 function init() {
     console.log('DOM');
+    function clickButton()  {
+        const spanIP = document.querySelector('span');
+    
+        const promise = fetch(
+             'https://api.ipify.org?format=json'
+             );
+             promise
+             .then(resp => {
+             if(resp.ok) { return resp.json(); 
+    
+            }
+             return Promise.reject(resp);})
+                .then((ip)=> {
+                 spanIP.innerText = ip.ip;
+                })
+                .catch(err => console.error(err))
+                .finally(() => {
+                console.log('Odpytywanie API zakończone!')
+                });
+    }
+    btn.addEventListener('click', clickButton)
 }
+
+
