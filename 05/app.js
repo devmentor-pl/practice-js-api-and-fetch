efe3ca2b2e852961179630070b2212e7b004563a
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-10-20 20:49:10 +0200

    Finish task 5

--- 05/app.js
+++ 05/app.js
@@ -1,37 +1,88 @@
-const apiUrl = 'http://localhost:3000/users';
+const apiUrl = "http://localhost:3000/users";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    loadUsers();
+  loadUsers();
+
+  const form = document.querySelector(".form");
+
+  if (form) {
+    form.addEventListener("submit", onSubmit);
+  }
 }
 
 function loadUsers() {
-    const promise = fetchGet(apiUrl);
+  const promise = performFetch(apiUrl);
 
-    promise
-        .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+  promise.then((data) => insertUsers(data)).catch((err) => console.error(err));
 }
 
-function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
+function performFetch(url, options) {
+  return fetch(url, options).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    }
 
-            return Promise.reject(resp);
-        });
+    return Promise.reject(resp);
+  });
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+  const ulElement = document.querySelector(".users");
+  ulElement.innerHTML = "";
+  usersList.forEach((user) => {
+    const liElement = document.createElement("li");
+    liElement.innerText = `${user.firstName} ${user.lastName}`;
 
-        ulElement.appendChild(liElement);
-    });
+    ulElement.appendChild(liElement);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const firstName = getInputValue(".form__field--first-name");
+  const lastName = getInputValue(".form__field--last-name");
+
+  if (!isValueValid(firstName)) {
+    alert("Proszę podać imię.");
+    return;
+  }
+
+  if (!isValueValid(lastName)) {
+    alert("Proszę podać nazwisko.");
+    return;
+  }
+
+  saveData(firstName, lastName)
+    .catch((err) => console.error(err))
+    .finally(loadUsers);
+}
+
+function saveData(firstName, lastName) {
+  const data = { firstName, lastName };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  return performFetch(apiUrl, options);
+}
+
+function getInputValue(selector) {
+  const elem = document.querySelector(selector);
+
+  if (elem) {
+    return elem.value;
+  }
+
+  return null;
+}
+
+function isValueValid(value) {
+  return value !== null && value.trim().length > 0;
 }
