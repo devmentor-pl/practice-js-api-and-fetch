38de05a6195e1b070585285d38030e1ea5148b5b
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-10-20 18:14:01 +0200

    Finish task 3

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,30 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log('DOM');
+  console.log("DOM");
+
+  const button = document.querySelector("button");
+
+  if (button) {
+    button.addEventListener("click", () => {
+      fetchIPAddress();
+    });
+  }
+}
+
+function fetchIPAddress() {
+  const aipUrl = "https://api.ipify.org/?format=json";
+
+  fetch(aipUrl)
+    .then((response) => response.json())
+    .then((json) => showIPAddress(json.ip))
+    .catch((err) => console.error(err));
+}
+
+function showIPAddress(ip) {
+  const spanElem = document.querySelector("span");
+
+  if (spanElem) {
+    spanElem.innerText = ip;
+  }
 }
