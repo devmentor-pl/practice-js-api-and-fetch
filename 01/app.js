c10c08457de73e45677f80313ce28f5c1f640bab
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-10-20 17:17:43 +0200

    Finish task 1

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,39 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+  const divList = document.querySelectorAll("div");
 
+  setBorderColorAsync(
+    divList[0],
+    "red",
+    firstElemSetBorderColorFinishCallback.bind(null, divList)
+  );
 }
 
 function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+  setTimeout(() => {
+    element.style.border = `3px solid ${color}`;
+
+    callback();
+  }, Math.random() * 3000);
+}
+
+function firstElemSetBorderColorFinishCallback(elemList) {
+  setBorderColorAsync(
+    elemList[1],
+    "blue",
+    secondElemSetBorderColorFinishCallback.bind(null, elemList)
+  );
+}
+
+function secondElemSetBorderColorFinishCallback(elemList) {
+  setBorderColorAsync(
+    elemList[2],
+    "green",
+    thirdElemSetBorderColorFinishCallback
+  );
+}
+
+function thirdElemSetBorderColorFinishCallback() {
+  console.log("finish");
 }
