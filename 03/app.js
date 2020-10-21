8bd3d842be6fa422b3c9e18d9293f88ec4ccd1c2
AnnaBuller <aniabela@wp.pl>
2020-10-21 18:51:09 +0200

    solve 03 task

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,21 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+    const spanEl = document.querySelector('span');
+    const btnEl = document.querySelector('button');
+
+    const promise = fetch('https://api.ipify.org?format=json');
+    promise
+        .then(res => {
+            if (res.ok) { return res.json(); }
+            return Promise.reject(res);
+        })
+        .then(ip => uploadIPOnClick(btnEl, spanEl, ip.ip))
+        .catch(err => console.error(err))
+}
+
+function uploadIPOnClick(submittingEl, ipContainer, ipAddress) {
+    submittingEl.addEventListener('click', function () {
+        return ipContainer.innerText = ipAddress
+    })
 }
