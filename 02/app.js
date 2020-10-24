a26240c6c123ad87770a8bd525b4362e37495396
AnnaBuller <aniabela@wp.pl>
2020-10-24 17:19:43 +0200

    correct 02 task

--- 02/app.js
+++ 02/app.js
@@ -1,29 +1,21 @@
 document.addEventListener('DOMContentLoaded', init);
 function init() {
     const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    })
+
+    setBorderColorAsync(divList[0], 'red')
+        .then(() => setBorderColorAsync(divList[1], 'blue'))
+        .then(() => setBorderColorAsync(divList[2], 'green'))
+        .catch(err => console.error(err))
 }
 
-function setBorderColorAsync(element, color, callback) {
+function setBorderColorAsync(element, color) {
     const promise = new Promise((resolve, reject) => {
-        if(element && element instanceof HTMLElement) {
-            if (callback && typeof callback === 'function') {
+        if (element && element instanceof HTMLElement) {
                 resolve(setTimeout(() => {
                     element.style.border = `3px solid ${color}`;
-                    callback();
                 }, Math.random() * 3000));
-            } else {
-                reject('Parametr ~callback~ mus być funkcją');
-            };
         } else {
-            reject(alert('Paremetr ~element~ musi być prawidłowym elementem DOM'))
+            reject('Paremetr ~element~ musi być prawidłowym elementem DOM')
         }
     })
     return promise
