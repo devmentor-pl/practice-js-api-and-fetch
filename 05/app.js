2cfd44c8cc04d8a8f34a784ca5814a134c4a692c
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-09 13:05:37 +0100

    Create POST method in data.json API Task 05

--- 05/app.js
+++ 05/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    addUser();
 }
 
 function loadUsers() {
@@ -17,7 +18,7 @@ function loadUsers() {
 function fetchGet(url) {
     return fetch(url)
         .then(resp => {
-            if(resp.ok) {
+            if (resp.ok) {
                 return resp.json();
             }
 
@@ -34,4 +35,39 @@ function insertUsers(usersList) {
 
         ulElement.appendChild(liElement);
     });
+}
+
+function addUser() {
+    const form = document.querySelector('form');
+    const firstNameInput = document.querySelector('.form__field--first-name');
+    const lastNameInput = document.querySelector('.form__field--last-name');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        //Poniższe rozwiązanie nie działa. Nie rozumiem dlaczego nie mogę użyć destrukturyzacji?
+        /*    const {
+               firstName,
+               lastName
+           } = e.target.elements; */
+
+
+        const data = {
+            firstName: firstNameInput.value,
+            lastName: lastNameInput.value,
+        }
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        fetch(apiUrl, options)
+            .then(resp => console.log(resp))
+            .catch(err => console.error(err))
+            .finally(loadUsers);
+
+    })
 }
