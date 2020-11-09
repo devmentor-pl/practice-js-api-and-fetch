9c932bc33a2c34ca3a7908d02b3738b9dacc9677
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-09 11:06:42 +0100

    Print IP fetched from API Task 03

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', init);
+const btn = document.querySelector('button');
+const span = document.querySelector('span');
 
 function init() {
     console.log('DOM');
+
+
+    btn.addEventListener('click', downloadIP);
+}
+
+const downloadIP = (e) => {
+    const promise = fetch('https://api.ipify.org?format=json');
+    promise
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+            return Promise.reject(resp);
+        })
+        .then(ip => printIP(ip))
+        .catch(err => console.error(err))
+        .finally(() => {
+            console.log('API downloaded')
+        })
+}
+
+const printIP = (ip) => {
+    span.textContent = ip.ip;
 }
