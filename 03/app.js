95375dedfd36f2e73ad9bbee49c5a2025a3e966d
gregor232 <ktotok@gmail.com>
2021-01-11 20:00:42 +0100

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+    const button = document.querySelector("button");
+    const span = document.querySelector("span");
+    
+    const promise = fetch("https://api.ipify.org?format=json")
+    .then(response => response.json())
+    .then(data => button.addEventListener('click', () => {
+        span.innerText = data.ip
+    }));
+        
     console.log('DOM');
 }
