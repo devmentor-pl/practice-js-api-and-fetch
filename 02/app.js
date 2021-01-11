728f5c5391ea070ee137bbb7fc2cecedc319e110
gregor232 <ktotok@gmail.com>
2021-01-11 18:57:27 +0100

    task 02 done

--- 02/app.js
+++ 02/app.js
@@ -2,62 +2,37 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const divList = document.querySelectorAll('div');
+    fireFunctions(divList)
     
-    /* setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    }); */
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        console.log("f")
-    })
-    setBorderColorAsync(divList[1], 'blue', function() {
-        console.log("f")
-    })
-    setBorderColorAsync(divList[2], 'green', function() {
-        console.log("f")
-    })
     
 }
 
-function setBorderColorAsync(element, color, callback) {
+function setBorderColorAsync(element, color) { //usunalem callback, bo nijak mi to pasuje do rozwiazania zadania, jesli chdzilo o inna konmcepcje to niestety moj blad
+    
     if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
         
-        const promise = new Promise((resolve, reject) => {
-            if(callback && typeof callback === 'function') {
-                resolve(setTimeout(() => {
-                    element.style.border = `3px solid ${color}`;
-                    callback();
-                }, Math.random() * 3000));
-            } else {
-            reject(alert('Parametr ~callback~ mus być funkcją'));
-            }
-           });
-        //promise.then(function(resp) {
-        //    console.log(resp);
-        //   });
-           
-        //promise.catch(function(err) {
-        //    console.log(err);
-        //   });
-           
-       /* if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
-            }, Math.random() * 3000);
-        } else {
-            alert('Parametr ~callback~ mus być funkcją');
-        } */
+        setTimeout(() => {
+            element.style.border = `3px solid ${color}`;
+            
+        }, Math.random() * 3000)
+        
     } else {
         alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
     }
 
     
        
+}
+function fireFunctions(divList) {
+    const promise = new Promise((resolve, reject) => {
+        if (divList){
+            resolve();
+        } else {
+            reject()
+        }
+       });
+    promise.then(setBorderColorAsync(divList[0], 'red'));
+    promise.then(setBorderColorAsync(divList[1], 'blue'));
+    promise.then(setBorderColorAsync(divList[2], 'green'));
+
 }
