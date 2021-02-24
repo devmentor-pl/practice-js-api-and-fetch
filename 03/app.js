a3d822becfc08dce33235d4ce2d31bd0390fe4a2
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2021-02-24 18:37:52 +0100

    Done exercises

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,31 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+    
+    
+    const button = document.querySelector('button');
+
+    button.addEventListener('click', getData);
+}
+
+
+function getData() {
+    const promise = fetch('https://api.ipify.org?format=json');
+    promise
+    .then((resp) => {
+        if(resp.ok) {
+            return resp.json();
+        }
+        return Promise.reject(resp);
+    })
+    .then((ip) => {
+        const spanElement = document.querySelector('span');
+        spanElement.innerText = ip.ip;
+    })
+    .catch(err => {console.log(err)})
+    .finally(() => {
+        console.log('Done');
+    });
+
+    return promise;
 }
