0cad46af473482848b2adf045e2afce078143926
Karolina Jedrajczyk <kalka@MacBook-Pro-Karolina.local>
2021-03-06 06:50:39 +0100

    message

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', init);
-
+const buttonEl = document.querySelector('button');
+const spanEl = document.querySelector('span');
 function init() {
     console.log('DOM');
+
+    buttonEl.addEventListener('click', getIP);
+}
+
+function getIP() {
+    fetch('https://api.ipify.org?format=json')
+    .then(resp => {
+        if(resp.ok) { return resp.json(); }
+        return Promise.reject(resp);
+    })
+    .then(data => {
+        spanEl.textContent = data.ip;
+        
+    })
+    .catch(err => console.error(err))
+    .finally(() => {console.log('Odpytywanie API zakończone')})
 }
