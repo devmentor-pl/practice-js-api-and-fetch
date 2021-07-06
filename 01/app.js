810aa8fc49ba874cd3ac7930df085cfe20e35fd1
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-07-06 16:14:10 +0200

    done task 1

--- 01/app.js
+++ 01/app.js
@@ -1,17 +1,41 @@
+/* W pliku app.js masz przygotowany kod, który ustawia kolor obramowania dla wyszukanych divów.
+
+Niestety kod ten posiada zagnieżdżenia, co zmniejsza jego czytelność i może doprowadzić do efektu callback hell.
+
+Twoim zadaniem jest przebudowanie kodu w taki sposób, aby zagnieżdzenia nie występowały. Utwórz odpowiednie funkcje, które rozwiążą problem.*/
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
 
+    //  setBorderColorAsync(divList[0], 'red', function() {
+    //   setBorderColorAsync(divList[1], 'blue', function() {
+    //   setBorderColorAsync(divList[2], 'green', function() {
+    //  console.log('finish');
+    //   });
+    //  });
+    //  });
+
+
+    setBorderColorAsync(divList[0], 'red', firstCallback);
+    function firstCallback() {
+        setBorderColorAsync(divList[1], 'blue', secondCallback);
+    }
+
+    function secondCallback() {
+        setBorderColorAsync(divList[2], 'green', thirdCallback);
+    }
+
+    function thirdCallback() {
+        console.log('finish');
+    }
 }
+
 
 function setBorderColorAsync(element, color, callback) {
     setTimeout(() => {
@@ -19,3 +43,4 @@ function setBorderColorAsync(element, color, callback) {
         callback();
     }, Math.random() * 3000);
 }
+
