fa7d072bcbb6b76506078daa7b64ce5752d8ff00
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-07-14 13:54:08 +0200

    done task 4

--- 04/app.js
+++ 04/app.js
@@ -1,5 +1,67 @@
+/* W tym zadaniu musisz pobrać aktualną pogodę dla podanych w polu formularza współrzędnych.
+
+Wykorzystaj do tego API o nazwie weatherbit.io, które umożliwia wykonać dziennie 500 odpytań w darmowym planie.
+
+Aby móc skorzystać z tego API musisz się zarejestrować, aby uzyskać tzw. key.
+
+Adres pod którym możemy pobierać dane o pogodzie to: https://api.weatherbit.io/v2.0/current?key=[key]&lat=[latitude]&lon=[longitude], gdzie:
+
+[key] - Twój identyfikator
+[latitude] - szerokość geograficzna
+[longitude] - długość geograficzna
+Przykładowe współrzędne to:
+
+Warszawa: 52.232222, 21.008333
+Kraków: 50.061389, 19.938333
+Wrocław: 51.11, 17.022222
+Zapoznaj się z dokumentacją, która prezentuje strukturę odpowiedzi. Znajdziesz tam także informacje jak pobierać dane w języku polskim.*/
+
 document.addEventListener('DOMContentLoaded', init);
+
 
 function init() {
     console.log('DOM');
+
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', getData);
 }
+
+function getData(e) {
+    e.preventDefault();
+
+    const latitude = document.querySelector('.form__field--lat');
+    const longitude = document.querySelector('.form__field--lng');
+
+    if (latitude && longitude) {
+        const lat = latitude.value;
+        const lng = longitude.value;
+
+        console.log(lat);
+        console.log(lng);
+
+        const weatherLat = document.querySelector('.weather__lat');
+        const weatherLng = document.querySelector('.weather__lng');
+        const weatherSummary = document.querySelector('.weather__summary');
+        const weatherTemperature = document.querySelector('.weather__temperature');
+
+
+        weatherLat.innerText = lat;
+        weatherLng.innerText = lng;
+
+
+        const key = '46ab6ee6f1824ef3bce1cae4ac3583f5';
+
+
+        const promise = fetch(`http://api.weatherbit.io/v2.0/current?key=${key}&lang=pl&units=I&lon=${lng}&lat=${lat}`);
+
+
+        promise
+            .then((response) => response.json())
+            .then(data => {
+                weatherSummary.innerText = data.data[0].weather.description
+                weatherTemperature.innerText = data.data[0].temp
+            });
+    }
+
+}
+
--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,21 @@
+/*W tym zadaniu będziesz potrzebować narzędzia JSON Server, który uruchomi lokalne API na podstawie pliku data.json.
+
+We wspomnianym pliku znajdują sie już dane, które są ładowane do HTML-a za pomocą kodu napisanego w app.js - o ile lokalne API zostało uruchomione.
+
+Przypominam, że można to zrobić w terminalu przy pomocy komendy: json-server ./data.json --watch.
+
+Twoim zadaniem będzie napisanie obsługi formularza, która pozwoli na dodawanie danych do naszego lokalnego API.
+
+Przypominam, że trzeba:
+
+wykorzystać odpowiednią metodę (POST)
+utworzyć obiekt na podstawie wysłanych przez formularz dancyh, który trzeba zamienić na format JSON (JSON.stringify())
+przekazanć odpowiedni nagłówek ('Content-Type': 'application/json')
+Po dodaniu kolejnego użytkownika należy zaktualizować widok przy pomocy funkcji loadUsers(), którą należy uruchomić w odpowiednim momencie np. finally(). */
+
+
+
+
 const apiUrl = 'http://localhost:3000/users';
 
 document.addEventListener('DOMContentLoaded', init);
