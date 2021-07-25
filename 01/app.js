3a52481048b34e8e78aee07bede813abd7e46f5e
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-07-25 19:23:08 +0100

    01 solved

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,69 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
-function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
-
-}
+/*
 
 function setBorderColorAsync(element, color, callback) {
     setTimeout(() => {
         element.style.border = `3px solid ${color}`;
         callback();
     }, Math.random() * 3000);
+}
+
+
+
+// original code
+function init() {
+    const divList = document.querySelectorAll("div");
+
+    setBorderColorAsync(divList[0], "red", function () {
+        setBorderColorAsync(divList[1], "blue", function () {
+            setBorderColorAsync(divList[2], "green", function () {
+                console.log("finish");
+            });
+        });
+    });
+}
+
+
+
+// functions as callbacks
+
+function init() {
+    const divList = document.querySelectorAll("div");
+
+    setBorderColorAsync(divList[0], "red", firstCallback);
+
+    function firstCallback() {
+        setBorderColorAsync(divList[1], "blue", secondCallback);
+    }
+
+    function secondCallback() {
+        setBorderColorAsync(divList[2], "green", thirdCallback);
+    }
+
+    function thirdCallback() {
+        console.log("finish");
+    }
+}
+
+*/
+
+// promises
+
+function init() {
+    const divList = document.querySelectorAll("div");
+
+    const setBorderColorAsync = (element, color) => {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                element.style.border = `3px solid ${color}`;
+                resolve();
+            }, Math.random() * 3000);
+        });
+        return promise;
+    };
+
+    setBorderColorAsync(divList[0], "red")
+        .then(() => setBorderColorAsync(divList[1], "blue"))
+        .then(() => setBorderColorAsync(divList[2], "green"));
 }
