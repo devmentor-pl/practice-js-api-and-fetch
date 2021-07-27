dc1b0872d43b358638a15c1e54d9531e4191faf4
Malgorzata Kowacka <kowackag@gmail.com>
2021-07-27 10:27:23 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,26 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+    const button = document.querySelector('button');
+    button.addEventListener('click', getIP)
+}
+
+function getIP() {
+    const promise = fetch('https://api.ipify.org?format=json');
+    promise
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+            return Promise.reject(resp);
+        })
+        .then(ip => showIP(ip))
+        .catch(err => console.log(err))
+        .finally(() => console.log('Odpytywanie API zakończone!'))
+}
+
+function showIP(ip) {
+    const iptext = ip.ip;
+    const spanEl = document.querySelector('span');
+    spanEl.innerText = iptext;
 }
