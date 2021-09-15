d2b17494fae7f8d992256ba259adf860ba1f3b09
Jakub Chrobak <jchrobakk@gmail.com>
2021-09-15 17:28:13 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
+
+const button = document.querySelector("button");
+const span = document.querySelector("span");
 
 function init() {
-    console.log('DOM');
+  console.log("DOM");
+  button.addEventListener("click", getIP);
+}
+
+function getIP() {
+  fetch("https://api.ipify.org?format=json")
+    .then((resp) => resp.json())
+    .then((data) => renderIP(data.ip));
+}
+
+function renderIP(ip) {
+  span.textContent = ip;
 }
