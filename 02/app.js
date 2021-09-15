78166cba974a06956e1905b84fc5223b2542ba96
Jakub Chrobak <jchrobakk@gmail.com>
2021-09-15 17:20:36 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,32 +1,27 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
-
+  const divList = document.querySelectorAll("div");
+  setBorderColorAsync(divList[0], "red")
+    .then(() => setBorderColorAsync(divList[1], "blue"))
+    .then(() => setBorderColorAsync(divList[2], "green"))
+    .then(() => console.log("finish"));
 }
 
-function setBorderColorAsync(element, color, callback) {
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        
-        if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
-            }, Math.random() * 3000);
-        } else {
-            alert('Parametr ~callback~ mus być funkcją');
-        }
+function setBorderColorAsync(element, color) {
+  const promise = new Promise(function (resolve, reject) {
+    if (element && element instanceof HTMLElement) {
+      // sprawdzam czy parametr jest elementem DOM, więcej:
+      // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+
+      setTimeout(() => {
+        element.style.border = `3px solid ${color}`;
+        resolve("ok");
+      }, Math.random() * 3000);
     } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
+      reject("Paremetr ~element~ musi być prawidłowym elementem DOM");
     }
+  });
+
+  return promise;
 }
