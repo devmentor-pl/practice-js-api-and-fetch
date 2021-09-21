46e6d2f797c510b4474b5fa7b96eaac068a6f196
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-09-21 23:02:04 +0200

    closes task 2

--- 02/app.js
+++ 02/app.js
@@ -2,31 +2,29 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
 
+    setBorderColorAsync(divList[0], 'red')
+        .then(() => setBorderColorAsync(divList[1], 'blue'))
+        .then(() => setBorderColorAsync(divList[2], 'green'))
+        .catch((err) => console.error(err))
+        .finally(() => console.log('finish'));
 }
 
-function setBorderColorAsync(element, color, callback) {
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        
-        if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
-            }, Math.random() * 3000);
-        } else {
-            alert('Parametr ~callback~ mus być funkcją');
-        }
-    } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
-    }
+const setBorderColorAsync = (element, color) => {
+    return new Promise((resolve, reject) => {
+        const time = setTime();
+        setTimeout(() => {
+            isHTMLEl(element) ?
+                resolve(changeBorderColor(element, color)) :
+                reject('Parametr ~element~ musi być prawidłowym elementem DOM');
+        }, time);
+    });
+}
+
+const setTime = () => Math.random() * 3000;
+
+const isHTMLEl = (el) => (el && el instanceof HTMLElement);
+
+const changeBorderColor = (element, borderColor) => {
+    element.style.border = `3px solid ${borderColor}`;
 }
