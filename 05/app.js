b7bd5f7ca2139496b5989d9d72acfea1457bf7de
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-11-05 20:48:07 +0100

    Almost fifth task

--- 05/app.js
+++ 05/app.js
@@ -3,7 +3,10 @@ const apiUrl = 'http://localhost:3000/users';
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+    
     loadUsers();
+    const submit = document.querySelector('.form');
+    submit.addEventListener('submit', addUser);
 }
 
 function loadUsers() {
@@ -34,4 +37,43 @@ function insertUsers(usersList) {
 
         ulElement.appendChild(liElement);
     });
+}
+
+function addUser(ev) {
+    ev.preventDefault();
+
+    const userData = getUserDates();
+    sendDatas(userData);
+}
+
+function sendDatas(userData) {
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(userData),
+        headers: {'Content-Type': 'application/json'}
+    }
+
+    const promise = fetch(apiUrl, options);
+
+    promise
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+            return Promise.reject(resp);
+        })
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+        .finally(() => loadUsers);
+}
+
+function getUserDates() {
+
+    const name = document.querySelector('.form__field--first-name').value;
+    const lastName = document.querySelector('.form__field--last-name').value;
+
+    // console.log(typeof name);
+    // console.log(typeof lastName);
+    return {name, lastName};
 }
