dde9f2544d7b0c9d0544c4e8b196b81c1b0b99aa
Rafał Kazik <kazik.raf@gmail.com>
2021-12-14 19:56:43 +0100

    third task

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,21 @@
-document.addEventListener('DOMContentLoaded', init);
+const checkMyIPBtn = document.querySelector(".ip-btn");
+const showMyIPSpan = document.querySelector(".ip-adress");
 
-function init() {
-    console.log('DOM');
-}
+const showMyIP = () => {
+  const getMyIPPromise = fetch("https://api.ipify.org?format=json");
+
+  getMyIPPromise
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      }
+      return Promise.reject(resp);
+    })
+    .then((ipObject) => (showMyIPSpan.innerText = ipObject["ip"]))
+    .catch((err) => console.error(err))
+    .finally(() => {
+      console.log(`Moje IP to: ${showMyIPSpan.textContent}`);
+    });
+};
+
+document.addEventListener("click", showMyIP);
