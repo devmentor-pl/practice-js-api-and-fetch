2a8d572fd130748ea35b12a465c1c7cdb65cc799
Rafał Kazik <kazik.raf@gmail.com>
2021-12-15 20:14:48 +0100

    fifth task

--- 05/app.js
+++ 05/app.js
@@ -1,37 +1,61 @@
-const apiUrl = 'http://localhost:3000/users';
+const apiUrl = "http://localhost:3000/users";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    loadUsers();
+  loadUsers();
+  addUsers();
 }
 
 function loadUsers() {
-    const promise = fetchGet(apiUrl);
+  const promise = fetchGet(apiUrl);
 
-    promise
-        .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+  promise.then((data) => insertUsers(data)).catch((err) => console.error(err));
 }
 
 function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
+  return fetch(url).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    }
+    return Promise.reject(resp);
+  });
+}
 
-            return Promise.reject(resp);
-        });
+function addUsers() {
+  const form = document.querySelector("form");
+
+  const firstNameInput = document.querySelector(".form__field--first-name");
+  const lastNameInput = document.querySelector(".form__field--last-name");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const userData = {
+      firstName: firstNameInput.value,
+      lastName: lastNameInput.value,
+    };
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch(apiUrl, options)
+      .then((resp) => console.log(resp))
+      .catch((err) => console.error(err))
+      .finally(loadUsers);
+  });
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+  const ulElement = document.querySelector(".users");
+  ulElement.innerHTML = "";
+  usersList.forEach((user) => {
+    const liElement = document.createElement("li");
+    liElement.innerText = `${user.firstName} ${user.lastName}`;
 
-        ulElement.appendChild(liElement);
-    });
+    ulElement.appendChild(liElement);
+  });
 }
