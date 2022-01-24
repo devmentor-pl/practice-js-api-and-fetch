661d13ffb20bbaee3bdd2bd8782dec17b4d7d1ce
Ewelina <ewelina@ekopacz.pl>
2022-01-24 23:15:44 +0100

    upgrade

--- 03/app.js
+++ 03/app.js
@@ -3,3 +3,24 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     console.log('DOM');
 }
+
+const addIp = ({ip}) => {
+    const spanEl = document.querySelector('span');
+    spanEl.innerText = ip;
+}
+
+const getIp = () => {
+    fetch('https://api.ipify.org?format=json')
+        .then(response => {
+            if(response.ok) {return response.json();}
+            return new Promise.reject(response)
+        })
+        .then(ip => addIp(ip))
+        .catch(error => console.error(error))
+}
+
+const buttonEl = document.querySelector('button');
+buttonEl.addEventListener('click', getIp);
+
+
+
