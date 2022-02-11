0bdd536d439c666f704508f7a2be1e24d77e6119
Magdalena Nolde <magdalena@nolde.de>
2022-02-11 16:53:19 +0100

    finish tasks api fetch

--- 04/app.js
+++ 04/app.js
@@ -1,13 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', init);
 const button = document.querySelector('.form__submit');
-// const weatherLatInfo = document.querySelector('.weather__lat')
-// const weatherLngInfo = document.querySelector('.weather__lng')
-// const weatherSummary = document.querySelector('.weather__summary')
-// const currentTemprature = document.querySelector('.weather__temperature')
+const weatherLatInfo = document.querySelector('.weather__lat')
+const weatherLngInfo = document.querySelector('.weather__lng')
+const weatherSummary = document.querySelector('.weather__summary')
+const currentTemprature = document.querySelector('.weather__temperature')
 const inputLng = document.querySelector('.form__field--lng');
 const inputLat = document.querySelector('.form__field--lat');
-console.log(inputLat);
 
 function init() {
     const button = document.querySelector('.form__submit');
@@ -16,32 +15,31 @@ function init() {
 }
 
 function loadWeather(e) {
-    e.preventDefault;
+    e.preventDefault();
     const inputValueLat = inputLat.value;
     const inputValueLng = inputLng.value;
     const key = 'e2243021f54e4ba1927cde30039992a3';
     const lang = "pl";
-    const promise = fetch(`https://api.weatherbit.io/v2.0/current?key=${key}&lat=${inputValueLat}&lon=${inputValueLng}`)
+    const units = "I";
+    const promise = fetch(`https://api.weatherbit.io/v2.0/current?key=${key}&lat=${inputValueLat}&lon=${inputValueLng}&lang=${lang}&units=${units}`)
     promise
         .then(resp => {
             if(resp.ok) { return resp.json(); }
             return Promise.reject(resp);
-    })
-        .then(data => console.log(data.data[0]))
+        })
+        .then(data => addWeatherInfo(data))
         .catch(err => console.error(err))
         .finally(() => {
             console.log('Odpytywanie API zakończone!')
     });
 }
 
-// function addWeatherInfo() {
-        // weatherLatInfo.textContent = inputValueLat;
-        // weatherLngInfo.textContent = inputValueLng;
-        // weatherSummary.textContent = data.weather.description;
-        // currentTemprature.textContent = data.temp;
-    // })
-// 
-// }
+function addWeatherInfo(data) {
+        weatherLatInfo.textContent = data.data[0].lat;
+        weatherLngInfo.textContent = data.data[0].lon;
+        weatherSummary.textContent = data.data[0].weather.description;
+        currentTemprature.textContent = data.data[0].temp;
+}
 
 
 
