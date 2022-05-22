deb3f77237e43fd2ab945dba4307f399341659bb
Andrzej <andis_as@wp.pl>
2022-05-22 19:54:54 +0200

    zad 1 - done

--- 01/app.js
+++ 01/app.js
@@ -1,16 +1,44 @@
 document.addEventListener('DOMContentLoaded', init);
 
+// callback hell
+// ------------------------
+// function init() {
+//     const divList = document.querySelectorAll('div');
+//     setBorderColorAsync(divList[0], 'red', function() {
+//         setBorderColorAsync(divList[1], 'blue', function() {
+//             setBorderColorAsync(divList[2], 'green', function() {
+//                 console.log('finish');
+//             })
+//         });
+//     });
+// }
+
+// function setBorderColorAsync(element, color, callback) {
+//     setTimeout(() => {
+//         element.style.border = `3px solid ${color}`;
+//         callback();
+//     }, Math.random() * 3000);
+// }
+
+
+// function as callback and as paramter
+// ------------------------
 function init() {
     const divList = document.querySelectorAll('div');
     
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+    setBorderColorAsync(divList[0], 'red', firstBorder);
 
+    function firstBorder() {
+        setBorderColorAsync(divList[1], 'blue', secondBorder)
+    }
+    function secondBorder() {
+        setBorderColorAsync(divList[1], 'blue', thirdBorder)
+    }
+    function thirdBorder() {
+            setBorderColorAsync(divList[2], 'green', function() {
+                console.log('finished');
+            })
+    }
 }
 
 function setBorderColorAsync(element, color, callback) {
@@ -19,3 +47,12 @@ function setBorderColorAsync(element, color, callback) {
         callback();
     }, Math.random() * 3000);
 }
+
+
+
+
+
+
+
+
+
