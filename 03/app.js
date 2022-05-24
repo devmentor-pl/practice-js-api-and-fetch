1e6b72d323d374348741f44db605e0b4792f0231
PiotrW <piotr.wojcikievvicz@gmail.com>
2022-05-24 13:51:21 +0200

    first commit

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,33 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
+    const btn = document.querySelector('button');
+    const ipField = document.querySelector('span');
+
+    btn.addEventListener('click', () => updateIP(ipField));
+}
+
+
+function updateIP(ipField) {
+
+    const promise = fetch('https://api.ipify.org?format=json');
+
+    promise
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json()
+            }
+            else {
+                return Promise.reject(resp);
+            }
+        })
+        .then(data => changeSpanText(ipField, data.ip))
+        .catch(err => console.log(err))
+        .finally(() => console.log('Zakonczono czytanie API'))
+}
+
+function changeSpanText(ipField, ip) {
+
+    ipField.innerText = ip;
+
 }
--- 04/app.js
+++ 04/app.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', init);
 
+730d1b62752a45d69ba4fa85b948cfd8
+
 function init() {
     console.log('DOM');
 }
+
+
+const promise = fetch(`https://api.weatherbit.io/v2.0/current?key=[key]&lat=[latitude]&lon=[longitude]`)
