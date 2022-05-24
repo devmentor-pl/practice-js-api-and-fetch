ba16834536b6ed0f75b0b0459b17fd4492d2534e
Piotr <piotr.wojcikievvicz@gmail.com>
2022-05-24 22:11:29 +0200

    finished task 04

--- 04/app.js
+++ 04/app.js
@@ -1,10 +1,78 @@
 document.addEventListener('DOMContentLoaded', init);
 
-730d1b62752a45d69ba4fa85b948cfd8
-
 function init() {
-    console.log('DOM');
+
+    const form = document.querySelector('form'); 
+    const outputFields = [...document.querySelectorAll('strong')];    
+    
+    form.addEventListener('submit', e => showWeatherInfo(e, outputFields));
 }
 
+function showWeatherInfo(e, outputFields) {
 
-const promise = fetch(`https://api.weatherbit.io/v2.0/current?key=[key]&lat=[latitude]&lon=[longitude]`)
+    e.preventDefault();
+
+    const [latInput, lonInput] = e.target.elements;   
+    const lat = latInput.value;
+    const lon = lonInput.value;
+    
+    if (isValidLatitude(lat) && isValidLongitude(lon)) {
+
+        const apiUrl = createUrl(lat, lon);
+        
+        loadData(apiUrl)    
+            .then(resp => displayData(resp.data[0], outputFields))
+            .catch(err => console.log(err))
+            .finally(() => console.log('Zakończono odczytywanie API'));
+    }
+    else {
+        e.target.reset();
+        alert('Wprowadź poprawne współrzędne geograficzne');
+    }
+}
+
+function createUrl(lat, lon) {
+    
+    const myKey = '730d1b62752a45d69ba4fa85b948cfd8';
+    const apiUrl = `https://api.weatherbit.io/v2.0/current?key=${myKey}&lat=${lat}&lon=${lon}&lang=pl`;
+
+    return apiUrl;
+}
+
+function loadData(apiUrl) {
+
+    return fetch(apiUrl)
+        .then(resp => {
+            if (resp.ok) { return resp.json();}
+            if(resp.status === 429) {
+                return Promise.reject('LIMIT EXCEEDED');
+            }            
+            return Promise.reject(`Kod błędu: ${resp.status}`);
+        })        
+}
+
+function displayData(data, outputFields) {
+    
+    const {lat, lon, temp, weather : {description} } = data;    
+    const arrData = [lat, lon, description, temp];
+         
+    outputFields.forEach((item, index) => item.innerText = arrData[index]);
+}
+
+function isValidLatitude(lat) {
+    
+    if ((isNaN(lat)) || (lat < -90 ) || (lat > 90)) {
+        return false;
+    }
+    
+    return true;    
+}
+
+function isValidLongitude(lon) {
+    
+    if ((isNaN(lon)) || (lon < -180 ) || (lon > 180)) {
+        return false;
+    }
+
+    return true;        
+}
