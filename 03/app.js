0a2f61643ca325bf59a47f3dd150bf8fd5b3e3b9
Andrzej <andis_as@wp.pl>
2022-05-24 19:31:44 +0200

    zad 3 - fetch & problem CROS

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,26 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-    console.log('DOM');
+    console.log('DOM')
+
+    const button = document.querySelector('button')
+    button.addEventListener('click', getAddress)
+
+    function getAddress() {
+        console.log('click')
+        fetch('https://www.ipify.org')
+            .then(response => {
+                console.log(response)
+                if(reponse.ok) {
+                    return reponse.json()
+                } 
+            })
+            .then(val => console.log(val))
+            .catch(err => console.log('Problem: ',err))
+    }
+
+    const span = document.querySelector('span')
+    span.innerHTML = '0.0.0.1'
+
+
 }
