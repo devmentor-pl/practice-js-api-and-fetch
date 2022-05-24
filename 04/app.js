093eed3dbaf472f520b4b8eb97fc3d44e300c53c
Andrzej <andis_as@wp.pl>
2022-05-24 20:57:57 +0200

    zad 4 - done

--- 04/app.js
+++ 04/app.js
@@ -11,35 +11,24 @@ function init() {
 
     function getWeather(e) {
         e.preventDefault()
-        console.log('submit')
 
         let latValue = lat.value
-        console.log(latValue)
         let longValue = long.value
-        console.log(longValue)
-
-        latValue = 52.2
-        longValue = 21.0
 
         const KEY = '87642df3e4584b76ae77b48f4d15eef3'
-        // const URL = 'https://api.weatherbit.io/v2.0/current?key=[key]&lat=[latitude]&lon=[longitude]'
         const URL = `https://api.weatherbit.io/v2.0/current?key=${KEY}&lat=${latValue}&lon=${longValue}`
 
         fetch(URL)
         .then(resp => resp.json())
         .then(data => {
-            console.log(data)
             const temp = data && data.data[0].temp
-            console.log( temp )
             const desc = data && data.data[0].weather.description
-            console.log( desc )
             
             setWeather(latValue, longValue, temp, desc)
         })
         .catch(err => console.log( err ))
-
-
     }
+
     function setWeather(latValue, longValue, temp, desc) {
         const weatherLat = document.querySelector('.weather__lat')
         const weatherLng = document.querySelector('.weather__lng')
@@ -51,5 +40,4 @@ function init() {
         weatherSummary.innerText = desc
         weatherTemperature.innerText = temp
     }
-
 }
--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,5 @@
-const apiUrl = 'http://localhost:3000/users';
+// const apiUrl = 'http://localhost:3000/users';
+const apiUrl = './data.json';
 
 document.addEventListener('DOMContentLoaded', init);
 
@@ -10,22 +11,27 @@ function loadUsers() {
     const promise = fetchGet(apiUrl);
 
     promise
+        .then(data => {
+            console.log(data.users)
+            return data.users
+        })
         .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+        .catch(err => console.error(err))
 }
 
 function fetchGet(url) {
     return fetch(url)
         .then(resp => {
+            console.log( resp )
             if(resp.ok) {
                 return resp.json();
             }
-
             return Promise.reject(resp);
         });
 }
 
 function insertUsers(usersList) {
+    console.log( usersList )
     const ulElement = document.querySelector('.users');
     ulElement.innerHTML = '';
     usersList.forEach(user => {
