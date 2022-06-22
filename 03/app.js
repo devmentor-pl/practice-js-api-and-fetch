dc93a6f3035236cdd305dced07c505ac2c40c65d
Mateusz Suplewski <matx3582@gmail.com>
2022-06-22 19:44:21 +0200

    Task 3 - Done

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,28 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    const buttonEl = document.querySelector('button');
+    buttonEl.addEventListener('click', getIpAddress);
+
+
+    function getIpAddress() {
+        const promise = fetch('https://api.ipify.org?format=json');
+
+        promise
+            .then(resp => {
+                if (resp.ok) return resp.json()
+                return Promise.reject(resp);
+            })
+            .then(ip => showIpAddressInSpan(ip.ip))
+            .catch(err => console.log(err))
+            .finally(() => {
+                console.log('Odczytywanie zakończone!')
+            });
+    }
+
+    function showIpAddressInSpan(ip) {
+        const spanEl = document.querySelector('span');
+        spanEl.innerText = ip;
+    }
 }
