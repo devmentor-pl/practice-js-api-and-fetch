c37e64a6a4916657b019e130b99f2e29f469e15d
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-02 18:13:09 +0200

    Finished task 05

--- 05/app.js
+++ 05/app.js
@@ -1,37 +1,57 @@
-const apiUrl = 'http://localhost:3000/users';
+const apiUrl = "http://localhost:3000/users";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    loadUsers();
+	loadUsers();
+	addUser();
 }
 
 function loadUsers() {
-    const promise = fetchGet(apiUrl);
+	const promise = fetchGet(apiUrl);
 
-    promise
-        .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+	promise.then((data) => insertUsers(data)).catch((err) => console.error(err));
 }
 
 function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
+	return fetch(url).then((resp) => {
+		if (resp.ok) {
+			return resp.json();
+		}
 
-            return Promise.reject(resp);
-        });
+		return Promise.reject(resp);
+	});
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+	const ulElement = document.querySelector(".users");
+	ulElement.innerHTML = "";
+	usersList.forEach((user) => {
+		const liElement = document.createElement("li");
+		liElement.innerText = `${user.firstName} ${user.lastName}`;
 
-        ulElement.appendChild(liElement);
-    });
+		ulElement.appendChild(liElement);
+	});
+}
+function addUser() {
+	const form = document.querySelector("form");
+
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		const { firstName, lastName } = e.target.elements;
+		const data = {
+			firstName: firstName.value,
+			lastName: lastName.value,
+		};
+
+		const options = {
+			method: "POST",
+			body: JSON.stringify(data),
+			headers: { "Content-Type": "application/json" },
+		};
+		fetch(apiUrl, options)
+			.then((resp) => console.log(resp))
+			.catch((err) => console.log(err))
+			.finally(loadUsers);
+	});
 }
