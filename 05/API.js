74045171ed464288a1622ae191c1b7552a159082
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-03 15:29:33 +0200

    Modified task 05

---
+++ 05/API.js
@@ -0,0 +1,29 @@
+class API {
+	constructor() {
+		this.url = "http://localhost:3000/users";
+	}
+
+	loadData() {
+		return this._fetch();
+	}
+	addData(data) {
+		const options = {
+			method: "POST",
+			body: JSON.stringify(data),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+		return this._fetch(options);
+	}
+	_fetch(options) {
+		return fetch(this.url, options).then((resp) => {
+			if (resp.ok) {
+				return resp.json();
+			}
+			return Promise.reject(resp);
+		});
+	}
+}
+
+export default API;
--- 05/app.js
+++ 05/app.js
@@ -1,57 +1,11 @@
-const apiUrl = "http://localhost:3000/users";
+import API from "./API.js";
+import Users from "./Users.js";
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-	loadUsers();
-	addUser();
-}
-
-function loadUsers() {
-	const promise = fetchGet(apiUrl);
-
-	promise.then((data) => insertUsers(data)).catch((err) => console.error(err));
-}
-
-function fetchGet(url) {
-	return fetch(url).then((resp) => {
-		if (resp.ok) {
-			return resp.json();
-		}
-
-		return Promise.reject(resp);
-	});
-}
-
-function insertUsers(usersList) {
-	const ulElement = document.querySelector(".users");
-	ulElement.innerHTML = "";
-	usersList.forEach((user) => {
-		const liElement = document.createElement("li");
-		liElement.innerText = `${user.firstName} ${user.lastName}`;
-
-		ulElement.appendChild(liElement);
-	});
-}
-function addUser() {
-	const form = document.querySelector("form");
-
-	form.addEventListener("submit", (e) => {
-		e.preventDefault();
-		const { firstName, lastName } = e.target.elements;
-		const data = {
-			firstName: firstName.value,
-			lastName: lastName.value,
-		};
-
-		const options = {
-			method: "POST",
-			body: JSON.stringify(data),
-			headers: { "Content-Type": "application/json" },
-		};
-		fetch(apiUrl, options)
-			.then((resp) => console.log(resp))
-			.catch((err) => console.log(err))
-			.finally(loadUsers);
-	});
+	const api = new API();
+	const users = new Users(api);
+	users.load();
+	users.add();
 }
