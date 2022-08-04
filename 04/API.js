d14f2b9423b0d6b409d3ce458e23b052efb6a44b
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-08-04 12:27:19 +0200

    Task 04 modified

--- 04/API.js
+++ 04/API.js
@@ -2,7 +2,6 @@ class API {
 	constructor() {
 		this.API_KEY = "1afb275531dc4926a413e2161557d1a5";
 		this.url = `https://api.weatherbit.io/v2.0/current?key=${this.API_KEY}&lang=pl&units=I`;
-		// &lat=${getLatValue()}&lon=${getLongValue()}`;
 	}
 
 	_fetch(lat = "", long = "", options = { method: "GET" }) {
@@ -16,29 +15,10 @@ class API {
 				}
 				return Promise.reject(resp);
 			})
-			.then((data) => {
-				this.insert(data.data[0].weather.description, data.data[0].temp);
-			})
-			.catch((err) => console.log(err))
-			.finally(() => {
-				console.log("odpytywanie zakończone");
-			});
 	}
 	get(latValue, lngValue) {
 		console.log(latValue, lngValue);
 		return this._fetch(latValue, lngValue);
-	}
-	insert(summary, temp) {
-		const latValue = document.querySelector(".form__field--lat").value;
-		const lngValue = document.querySelector(".form__field--lng").value;
-		const latText = document.querySelector(".weather__lat");
-		const lngText = document.querySelector(".weather__lng");
-		const summaryItem = document.querySelector(".weather__summary");
-		const tempItem = document.querySelector(".weather__temperature");
-		latText.innerText = latValue;
-		lngText.innerText = lngValue;
-		summaryItem.innerText = summary;
-		tempItem.innerText = temp;
 	}
 }
 export default API;
--- 04/app.js
+++ 04/app.js
@@ -3,31 +3,35 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
 	const form = document.querySelector("form");
-
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
 		const latValue = document.querySelector(".form__field--lat").value;
 		const lngValue = document.querySelector(".form__field--lng").value;
 		const api = new API();
-		api.get(latValue, lngValue)
+		api
+			.get(latValue, lngValue)
+			.then((data) => {
+				insertData(
+					latValue,
+					lngValue,
+					data.data[0].weather.description,
+					data.data[0].temp
+				);
+			})
+			.catch((err) => console.log(err))
+			.finally(() => {
+				console.log("odpytywanie zakończone");
+			});
 	});
+}
 
-	// function getCoordinates() {
-	// 	const latValue = document.querySelector(".form__field--lat").value;
-	// 	const lngValue = document.querySelector(".form__field--lng").value;
-	// 	return {
-	// 		latitude: latValue,
-	// 		longditude: lngValue,
-	// 	};
-	// }
-	// function insertData(latitude, longditude, summary, temp) {
-	// 	const latText = document.querySelector(".weather__lat");
-	// 	const lngText = document.querySelector(".weather__lng");
-	// 	const summaryItem = document.querySelector(".weather__summary");
-	// 	const tempItem = document.querySelector(".weather__temperature");
-	// 	latText.innerText = latitude;
-	// 	lngText.innerText = longditude;
-	// 	summaryItem.innerText = summary;
-	// 	tempItem.innerText = temp;
-	// }
+function insertData(latitude, longditude, summary, temp) {
+	const latText = document.querySelector(".weather__lat");
+	const lngText = document.querySelector(".weather__lng");
+	const summaryItem = document.querySelector(".weather__summary");
+	const tempItem = document.querySelector(".weather__temperature");
+	latText.innerText = latitude;
+	lngText.innerText = longditude;
+	summaryItem.innerText = summary;
+	tempItem.innerText = temp;
 }
