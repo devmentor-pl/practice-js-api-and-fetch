4c3bb3d8d3bfb5307ba0d4ee3e2cb44eac2a56ef
Marta Stelmach <marta.ms1961@gmail.com>
2022-08-16 17:11:24 +0200

    Finish task-3

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,25 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
-}
+    const buttonElement = document.querySelector('button');
+    if (buttonElement) {
+        buttonElement.addEventListener(
+            'click',
+            getIp
+        );
+    };
+
+    function getIp() {
+        const promise = fetch('https://api.ipify.org?format=json');
+        promise.then(response => {
+            if (response.ok) {
+                return response.json();
+            };
+        }).then(data => {
+            const spanElement = document.querySelector('span');
+            if (spanElement) {
+                spanElement.innerText = data.ip;
+            };
+        }).catch(error => console.error("Błąd w pobieraniu danych.", error));
+    };
+};
