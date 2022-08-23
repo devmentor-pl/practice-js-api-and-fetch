86131dda7ea785f2e8f2e5824104f74cf8911b08
Dawid Drag <dede7d@wp.pl>
2022-08-23 18:27:40 +0200

    task5 done

--- 05/app.js
+++ 05/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    addUsers()
 }
 
 function loadUsers() {
@@ -17,7 +18,7 @@ function loadUsers() {
 function fetchGet(url) {
     return fetch(url)
         .then(resp => {
-            if(resp.ok) {
+            if (resp.ok) {
                 return resp.json();
             }
 
@@ -34,4 +35,35 @@ function insertUsers(usersList) {
 
         ulElement.appendChild(liElement);
     });
+}
+
+
+function addUsers() {
+
+    const form = document.querySelector('form')
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+
+        console.log(event.target.elements)
+
+        const firstName = event.target.elements[0]
+        const lastName = event.target.elements[1]
+
+        const data = {
+            firstName: firstName.value,
+            lastName: lastName.value
+        }
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        }
+
+        fetch(apiUrl, options)
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err))
+            .finally(loadUsers)
+    })
+
 }
