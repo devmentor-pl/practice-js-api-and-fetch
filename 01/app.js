b861e056b307a28fbb2c9cb083e3a91c00ab206c
Karolina Grzywa <grzywakarolina@gmail.com>
2022-08-31 14:18:06 +0200

    Test task 1

--- 01/app.js
+++ 01/app.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
+
+    setBorderColorAsync(divList[0], 'red', function () {
+        setBorderColorAsync(divList[1], 'blue', function () {
+            setBorderColorAsync(divList[2], 'green', function () {
                 console.log('finish');
             });
         });
@@ -19,3 +19,42 @@ function setBorderColorAsync(element, color, callback) {
         callback();
     }, Math.random() * 3000);
 }
+// async
+
+function getAsyncData(callback) {
+    const time = Math.random() * 5000;
+    const insideData = { name: 'secret', value: null };
+    setTimeout(function () {
+        callback(insideData)
+    }, time);
+}
+
+console.log('before');
+getAsyncData(function (data1) {
+    console.log('async 1', data1);
+});
+getAsyncData(function (data2) {
+    console.log('async 2', data2);
+});
+console.log('after');
+
+
+
+//promise
+const a = 12, b = 1;
+const promise = new Promise(function (resolve, reject) {
+    if (b !== 0) {
+        resolve(a / b);
+    } else {
+        reject('nie dzielimy przez 0');
+    }
+
+});
+
+promise.then(function (resp) {
+    console.log(resp)
+});
+
+promise.catch(function (err) {
+    console.log(err)
+})
