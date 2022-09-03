bbeeb2344961a0e4df220401aabb1037a72b06d5
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2022-09-03 19:58:12 +0200

    task completed

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
+    const button = document.querySelector('button')
+    button.addEventListener('click', getIP)
+}
+
+const getIP = () => {
+    const span = document.querySelector('span')
+    const promise = fetch('https://api.ipify.org?format=json')
+    promise.then(resp => {
+        if (resp.ok) {
+            return resp.json()
+        }
+    })
+        .then(data => {
+            if (span) {
+                span.innerText = data.ip
+            }
+            else {
+                console.error('Error')
+            }
+
+        })
+        .catch(err => {
+            console.error('Error')
+        })
+        .finally(() => console.log('Finished'))
+
+
 }
