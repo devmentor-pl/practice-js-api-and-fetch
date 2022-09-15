246176811b270f83ccb0dbefa9e9405b81e53995
Zuza Ganobis <zuzaganobis@gmail.com>
2022-09-15 18:48:20 +0200

    Completed tasks

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,33 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+  const divList = document.querySelectorAll("div");
 
+  // setBorderColorAsync(divList[0], "red", function () {
+  //   setBorderColorAsync(divList[1], "blue", function () {
+  //     setBorderColorAsync(divList[2], "green", function () {
+  //       console.log("finish");
+  //     });
+  //   });
+  // });
+  setBorderColorAsync(divList[0], 0, setSecondBorderColor);
+  function setSecondBorderColor() {
+    setBorderColorAsync(divList[0], "red", setThirdBorderColor);
+  }
+  function setThirdBorderColor() {
+    setBorderColorAsync(divList[1], "blue", setFourthBorderColor);
+  }
+  function setFourthBorderColor() {
+    setBorderColorAsync(divList[2], "green", setFifthBorderColor);
+  }
+  function setFifthBorderColor() {
+    console.log("finish");
+  }
 }
 
 function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+  setTimeout(() => {
+    element.style.border = `3px solid ${color}`;
+    callback();
+  }, Math.random() * 3000);
 }
--- 02/app.js
+++ 02/app.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+  const divList = document.querySelectorAll("div");
 
+  // setBorderColorAsync(divList[0], "red", function () {
+  //   setBorderColorAsync(divList[1], "blue", function () {
+  //     setBorderColorAsync(divList[2], "green", function () {
+  //       console.log("finish");
+  //     });
+  //   });
+  // });
+  setBorderColorAsync(divList[0], "red")
+    .then(() => setBorderColorAsync(divList[1], "blue"))
+    .then(() => setBorderColorAsync(divList[2], "green"))
+    .catch(() => console.error(err))
+    .finally(() => console.log("finish"));
 }
 
-function setBorderColorAsync(element, color, callback) {
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        
-        if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
-            }, Math.random() * 3000);
-        } else {
-            alert('Parametr ~callback~ mus być funkcją');
-        }
+function setBorderColorAsync(element, color) {
+  const promise = new Promise((resolve, reject) => {
+    const time = Math.random() * 3000;
+
+    if (element && element instanceof HTMLElement) {
+      setTimeout(() => {
+        element.style.border = `3px solid ${color}`;
+        resolve();
+      }, time);
     } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
+      reject(alert("Paremetr ~element~ musi być prawidłowym elementem DOM"));
     }
+  });
+  return promise;
 }
