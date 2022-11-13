54bcfee2fb8b76e71c8e41642888b79715131660
Jacek Zielinski <gandi34@wp.pl>
2022-11-13 13:21:21 +0100

    Api and Fetch practice

--- 03/app.js
+++ 03/app.js
@@ -3,3 +3,16 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     console.log('DOM');
 }
+
+function getApi () {
+    const promise = fetch('https://api.ipify.org');
+    promise
+        .then(resp => resp.text())
+        .then(ip => console.log(ip))
+        .catch(err => console.error(err))
+        .finally(() => {
+            console.log('Odpytywanie API zakończone!')
+});
+}
+   
+getApi();
