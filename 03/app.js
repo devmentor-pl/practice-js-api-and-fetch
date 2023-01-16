605c7eb438be918d78945388f856dd6baaa1a33e
Anna <aniamatrackasadurek@gmail.com>
2023-01-16 11:36:00 +0100

    exercise 3 finished

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,25 @@
-document.addEventListener('DOMContentLoaded', init);
-
-function init() {
+const init = () => {
     console.log('DOM');
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', getIP)
 }
+
+const getIP = () => {
+    const promise = fetch('https://api.ipify.org?format=json');
+    promise
+    .then(resp => {
+        if (resp.ok) {return resp.json(); }
+        return Promise.reject(resp);
+    })
+    .then(ip => showIP(ip))
+    .catch(err => console.log(err))
+    .finally( () => console.log('IP address downloaded successfully'))
+}
+
+function showIP(ip) {
+    const spanEl = document.querySelector('span');
+    const ipAddress = ip.ip;
+    spanEl.innerText = ipAddress;
+}
+
+document.addEventListener('DOMContentLoaded', init);
