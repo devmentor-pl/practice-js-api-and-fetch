1f1f6060c60503377171783648de01a1e3d15865
Michal <maxpoz3212@gmail.com>
2023-02-02 15:45:27 +0100

    Add code refactoring in task 04

--- 04/modules/API.js
+++ 04/modules/API.js
@@ -1,5 +1,98 @@
 class API {
+    constructor() {
+        // to check real time data set "this.API.enabled" to "true"
+        this.API = {
+            url: 'https://api.weatherbit.io/v2.0/current?key=ffc13da65b494013b12370e7bdecb3f0',
+            enabled: false,
+        };
+        this.dataForAPI = {
+            lat: '',
+            lng: '',
+            units: 'I',
+            lang: 'pl',
+        };
+        this.apiProto = {
+            "data": {
+                "wind_cdir": "NE",
+                "rh": 59,
+                "pod": "d",
+                "lon": -78.63861,
+                "pres": 1006.6,
+                "timezone": "America\/New_York",
+                "ob_time": "2017-08-28 16:45",
+                "country_code": "US",
+                "clouds": 75,
+                "vis": 10,
+                "wind_spd": 6.17,
+                "gust": 8,
+                "wind_cdir_full": "northeast",
+                "app_temp": 24.25,
+                "state_code": "NC",
+                "ts": 1503936000,
+                "h_angle": 0,
+                "dewpt": 15.65,
+                "weather": {
+                    "icon": "c03d",
+                    "code": 803,
+                    "description": "Broken clouds"
+                },
+                "uv": 2,
+                "aqi": 45,
+                "station": "CMVN7",
+                "sources": ["rtma", "CMVN7"],
+                "wind_dir": 50,
+                "elev_angle": 63,
+                "datetime": "2017-08-28:17",
+                "precip": 0,
+                "ghi": 444.4,
+                "dni": 500,
+                "dhi": 120,
+                "solar_rad": 350,
+                "city_name": "Raleigh",
+                "sunrise": "10:44",
+                "sunset": "23:47",
+                "temp": 24.19,
+                "lat": 35.7721,
+                "slp": 1022.2
+            }
+        };
+    }
 
+    getGeoCoords(geoCoordsFromInputs) {
+        const { lat, lng } = geoCoordsFromInputs;
+        this.dataForAPI.lat = lat;
+        this.dataForAPI.lng = lng;
+    }
+
+    loadData() {
+        return this._fetch();
+    }
+
+    _fetch() {
+        const { lat, lng, units, lang } = this.dataForAPI;
+        const url = `${this.API.url}&lat=${lat}&lon=${lng}&units=${units}&lang=${lang}`;
+
+        if (this._isApiEnabled()) {
+            return fetch(url)
+                .then(resp => {
+                    if (resp.ok) {
+                        return resp.json();
+                    }
+                    return Promise.reject(resp);
+                })
+                .then(weatherData => weatherData.data[0])
+        } else {
+            return new Promise((resolve, reject) => {
+                resolve(this.apiProto.data);
+            });
+        }
+    }
+
+    _isApiEnabled() {
+        if (this.API.enabled) {
+            return true
+        }
+    }
 }
 
 export default API;
--- 04/modules/WeatherGUI.js
+++ 04/modules/WeatherGUI.js
@@ -1,4 +1,166 @@
 class WeatherGUI {
+    constructor(api) {
+        this.api = api;
+        this.formEl = document.querySelector('.form');
+        this.weatherInfoElement = document.querySelector('.weather');
+    }
+
+    load() {
+        this.getFormData();
+    }
+
+    getFormData() {
+        this.formEl.addEventListener('submit', e => this.handleSubmit(e));
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const errors = [];
+
+        this.validateInputsData(errors);
+
+        if (this.isElementEmpty(errors)) {
+            const { lat, lng } = this.formEl.elements;
+            const geoCoords = {
+                lat: lat.value,
+                lng: lng.value,
+            }
+
+            this.sendToApi(geoCoords)
+            this.loadApiData();
+            this.clearErrorsList();
+        }
+
+        this.renderErrors(errors);
+    }
+
+    validateInputsData(errors) {
+        const fields = [
+            { name: 'lat', label: 'latitude', required: true, pattern: '^-?([0-9]{1,2}|1[0-7][0-9]|180)(\.[0-9]{1,10})$', },
+            { name: 'lng', label: 'longitude', required: true, pattern: '^-?([0-9]{1,2}|1[0-7][0-9]|180)(\.[0-9]{1,10})$', },
+        ];
+
+        fields.forEach(field => {
+            const { name, label, required, pattern } = field;
+
+            const inputValue = this.formEl.elements[name].value;
+
+            if (required) {
+                if (this.isElementEmpty(inputValue)) {
+                    errors.push(`Pole ${label.toUpperCase()} jest wymagane.`);
+                }
+            }
+            if (inputValue.length > 0 && pattern) {
+                if (this.isNotValidPattern(pattern, inputValue)) {
+                    errors.push(`Wprowadź poprawną wartość w polu ${label} (np. "52.232222").`);
+                }
+            }
+        })
+    }
+
+    isNotValidPattern(pattern, inputValue) {
+        const reg = new RegExp(pattern);
+        if (!reg.test(inputValue)) { return true }
+    }
+
+    sendToApi(data) {
+        this.api.getGeoCoords(data);
+    }
+
+    loadApiData() {
+        this.api.loadData()
+            .then(data => this.insertData(data))
+            .catch(err => console.error(err));
+    }
+
+    insertData(weatherData) {
+        const { lon, lat, temp, city_name, weather: { description } } = weatherData;
+        const [parentEl] = this.weatherInfoElement.children;
+        const [latEl, lngEl, cityEl, descriptionEl, temperatureEl] = parentEl.children;
+
+        const infoElementsArr = [
+            { element: latEl, text: lat },
+            { element: lngEl, text: lon },
+            { element: cityEl, text: `${city_name}` },
+            { element: descriptionEl, text: description },
+            { element: temperatureEl, text: temp },
+        ];
+
+        this.renderWeatherData(infoElementsArr);
+    }
+
+    renderWeatherData(infoElementsArr) {
+        infoElementsArr.forEach(infoElement => {
+            const { element, text } = infoElement;
+
+            element.innerText = text;
+        });
+    }
+
+    renderErrors(errors) {
+        if (!this.isElementEmpty(errors)) {
+            this.renderFormErrors(errors);
+            this.renderSectionError();
+        }
+    }
+
+    renderFormErrors(errors) {
+        const errorsList = this.createList(errors, 'form__errors-list', 'form__errors-list-item');
+        errorsList.style.color = 'red';
+        errorsList.style.listStyle = 'none';
+
+        this.clearErrorsList();
+
+        this.formEl.insertBefore(errorsList, this.formEl.lastElementChild);
+    }
+
+    renderSectionError() {
+        const errorEl = this.createElement('p', 'weather__error');
+
+        this.weatherInfoElement.children[0].style.display = 'none';
+
+        errorEl.innerText = 'Wprowadź poprawne dane i spróbuj ponownie.';
+        this.weatherInfoElement.appendChild(errorEl);
+    }
+
+    clearErrorsList() {
+        this.weatherInfoElement.children[0].style.display = 'block';
+
+        this.removeElement(this.weatherInfoElement, '.weather__error')
+        this.removeElement(this.formEl, '.form__errors-list');
+    }
+
+    isElementEmpty(element) {
+        if (element.length === 0) return true;
+    }
+
+    createList(itemsContent, listClass, itemClass) {
+        const listEl = this.createElement('ul', listClass);
+
+        itemsContent.forEach((itemContent) => {
+            const listItem = this.createElement('li', itemClass);
+
+            listItem.innerText = itemContent;
+
+            listEl.appendChild(listItem);
+        });
+
+        return listEl;
+    }
+
+    createElement(element, className) {
+        const newElement = document.createElement(element);
+
+        newElement.classList.add(className);
+
+        return newElement;
+    }
+
+    removeElement(parentEl, selectorToRemove) {
+        const elementToRemove = parentEl.querySelector(selectorToRemove);
+
+        if (elementToRemove) parentEl.removeChild(elementToRemove);
+    }
 
 }
 
