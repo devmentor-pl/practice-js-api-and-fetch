92b44419a21c6395654c9702d5f3ed5fff5de620
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-02-10 22:03:15 +0100

    03 done

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', init);
+const promise = fetch('https://api.ipify.org?format=json');
+
+
 
 function init() {
-    console.log('DOM');
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', () => {
+        promise
+            .then(resp => {
+                if (resp.ok) {
+                    return resp.json();
+                }
+                return Promise.reject(resp);
+            })
+            .then(ip => {
+                document.querySelector('span').textContent = ip.ip;
+            })
+            .catch(err => console.error(err));
+    })
+
 }
