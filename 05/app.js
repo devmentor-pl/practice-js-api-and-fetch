8080d59ceaa8466f790ec63ba38bfc8d1cd02ee9
Martyna Giee <martyna.giee@gmail.com>
2023-02-19 10:45:16 +0100

    task 5 done

--- 05/app.js
+++ 05/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    addUser();
 }
 
 function loadUsers() {
@@ -17,7 +18,7 @@ function loadUsers() {
 function fetchGet(url) {
     return fetch(url)
         .then(resp => {
-            if(resp.ok) {
+            if (resp.ok) {
                 return resp.json();
             }
 
@@ -34,4 +35,28 @@ function insertUsers(usersList) {
 
         ulElement.appendChild(liElement);
     });
+}
+
+function addUser() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+
+        const data = {
+            firstName: e.target.elements[0].value,
+            lastName: e.target.elements[1].value
+        }
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        }
+
+        fetch(apiUrl, options)
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err))
+            .finally(loadUsers)
+    })
+
 }
