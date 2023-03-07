8aa7c6d8c0fe0738d6da3d0341f45f39175999e1
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-03-07 12:35:09 +0100

    Add ex 1

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,26 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+	const divList = document.querySelectorAll("div");
 
+	setBorderColorAsync(divList[0], "red", changeToBlue);
+
+	function changeToBlue() {
+		setBorderColorAsync(divList[1], "blue", changeToGreen);
+	}
+
+	function changeToGreen() {
+		setBorderColorAsync(divList[2], "green", showConsoleLog);
+	}
+
+	function showConsoleLog() {
+		console.log("finish");
+	}
 }
 
 function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+	setTimeout(() => {
+		element.style.border = `3px solid ${color}`;
+		callback();
+	}, Math.random() * 3000);
 }
