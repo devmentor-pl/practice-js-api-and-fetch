6985624f7980622bbedd2cf2641fc33784d06011
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-03-08 11:29:20 +0100

    Add ex 3

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,25 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log('DOM');
+	console.log("DOM");
+	const button = document.querySelector("button");
+	button.addEventListener("click", getIP);
+}
+
+function getIP() {
+	const promise = fetch("https://api.ipify.org?format=json");
+
+	promise
+		.then(resp => {
+			if (resp.ok) {
+				return resp.json();
+			}
+			return Promise.reject(resp);
+		})
+		.then(resp => {
+			const span = document.querySelector("span");
+			span.innerText = resp.ip;
+		})
+		.catch(err => console.error(err))
+		.finally(() => console.log("IP zostało pobrane"));
 }
