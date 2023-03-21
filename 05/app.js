72c4d8c4e2d50437ce0a315eaed49dc7375205d1
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-03-21 22:10:46 +0100

    task-05 done

--- 05/app.js
+++ 05/app.js
@@ -1,37 +1,69 @@
-const apiUrl = 'http://localhost:3000/users';
+const apiUrl = 'http://localhost:3000/users'
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-    loadUsers();
+	const formEl = document.querySelector('form')
+	if (formEl) {
+		formEl.addEventListener('submit', handleSubmit)
+	}
+	loadUsers()
 }
 
 function loadUsers() {
-    const promise = fetchGet(apiUrl);
+	const promise = fetchGet(apiUrl)
 
-    promise
-        .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+	promise.then((data) => insertUsers(data)).catch((err) => console.error(err))
 }
 
 function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
+	return fetch(url).then((resp) => {
+		if (resp.ok) {
+			return resp.json()
+		}
 
-            return Promise.reject(resp);
-        });
+		return Promise.reject(resp)
+	})
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+	const ulElement = document.querySelector('.users')
+	ulElement.innerHTML = ''
+	usersList.forEach((user) => {
+		const liElement = document.createElement('li')
+		liElement.innerText = `${user.firstName} ${user.lastName}`
 
-        ulElement.appendChild(liElement);
-    });
+		ulElement.appendChild(liElement)
+	})
+}
+
+function handleSubmit(e) {
+	e.preventDefault()
+
+	const firstName = e.target.elements[0]
+	const lastName = e.target.elements[1]
+
+	if (firstName.value.length > 3 && lastName.value.length > 3) {
+		const data = {
+			firstName: firstName.value,
+			lastName: lastName.value,
+		}
+
+		addUser(data)
+	} else {
+		alert('Enter correct first and last name!')
+	}
+}
+
+function addUser(newUser) {
+	const options = {
+		method: 'POST',
+		body: JSON.stringify(newUser),
+		headers: { 'Content-Type': 'application/json' },
+	}
+
+	fetch(apiUrl, options)
+		.then((resp) => console.log(resp))
+		.catch((err) => console.log(err))
+		.finally(loadUsers())
 }
