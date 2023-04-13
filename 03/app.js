7c00af415fa456f392d454fc2c297eb523536bbb
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-04-13 21:08:24 +0200

    zadania kontrolne

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,29 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
+const btnEl = document.querySelector("button");
+const spanEl = document.querySelector("span");
 
 function init() {
-    console.log('DOM');
+  console.log("DOM");
+  loadIp();
+}
+
+function loadIp() {
+  fetch("https://api.ipify.org?format=json")
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      }
+      return Promise.reject(resp);
+    })
+    .then((ip) => insertIp(ip))
+    .catch((err) => console.error(err))
+    .finally(() => {
+      console.log("Api zakończyło działanie");
+    });
+}
+
+function insertIp(ip) {
+  btnEl.addEventListener("click", () => {
+    spanEl.innerHTML = ip.ip;
+  });
 }
