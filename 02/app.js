f71cb6ec95f6cdab33fad3ff9e8211f5d38f367a
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-05-01 21:46:53 +0200

    finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,32 +1,33 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
     
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
-
-}
-
-function setBorderColorAsync(element, color, callback) {
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+    const divList = document.querySelectorAll('div');
+    const setBorderColorAsync = (element, color) => {
+        const promise =  new Promise((resolve, reject) => {
+            if(element && element instanceof HTMLElement) {
+                setTimeout(() => {
+                    element.style.border = `3px solid ${color}`;
+                    resolve('jest ok');
+                }, Math.random() * 3000);
         
-        if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
-            }, Math.random() * 3000);
-        } else {
-            alert('Parametr ~callback~ mus być funkcją');
-        }
-    } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
+            } else {
+                reject('Paremetr ~element~ musi być prawidłowym elementem DOM');
+            }
+        });
+        
+        return promise;
     }
+
+
+    setBorderColorAsync(divList[0], 'red', setBorderColorAsync)
+        .then(() => setBorderColorAsync(divList[1], 'blue'))
+        .then(() => setBorderColorAsync(divList[2], 'green'))
+        .then(()=> console.log('finish'))
+        .catch(err => console.log(err))
+
+    
 }
+
+
+
