9437e3ecea1d7726fe5d1a65b63eb0f4ab78a04f
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-05-01 22:31:54 +0200

    finish task 3

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,26 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    const insertIP = () => {
+        const promise =  fetch('https://api.ipify.org');
+        promise
+            .then(resp => {
+                if(resp.ok) {
+                    return resp.text()
+                }
+
+                return Promise.reject(resp)  
+            })
+            .then(ip => spanEl.innerText = ip)
+            .catch(err => console.error(err))
+            .finally(() => console.log("Zapytanie api zakończone!"))
+    }
+
+    const spanEl = document.querySelector('span');
+    const buttonEl = document.querySelector('button');
+
+    buttonEl.addEventListener('click', insertIP)
+
+
 }
