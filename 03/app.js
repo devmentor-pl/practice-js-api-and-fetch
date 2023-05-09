32813243b6253d65272d546d8e22a794b8d48c37
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-05-09 12:59:09 +0200

    Finished task 03

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,25 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    const btnEl = document.querySelector('button');
+    const spanEl = document.querySelector('span');
+
+    if(btnEl){
+        btnEl.addEventListener('click', getIp())
+    }
+    function getIp(){
+        fetch('https://api.ipify.org?format=json')
+        .then (resp => {
+            if(resp.ok){
+                return resp.json()
+            }
+        }).then(data => {
+            if(spanEl){
+                spanEl.innerText = data.ip
+            }else{
+                console.error('Nie mogę znaleźć elementu!')
+            }
+        }).catch(err => console.log('Nie mogę pobrać danych.', err))
+    }
 }
