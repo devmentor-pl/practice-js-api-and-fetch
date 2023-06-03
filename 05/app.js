8cda42306dc3a9ddf3fc44ede1bdff113cc80f42
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-06-03 11:59:03 +0200

    Task 5 added

--- 05/app.js
+++ 05/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    addUser();
 }
 
 function loadUsers() {
@@ -17,7 +18,7 @@ function loadUsers() {
 function fetchGet(url) {
     return fetch(url)
         .then(resp => {
-            if(resp.ok) {
+            if (resp.ok) {
                 return resp.json();
             }
 
@@ -34,4 +35,41 @@ function insertUsers(usersList) {
 
         ulElement.appendChild(liElement);
     });
+}
+
+function addUser() {
+    const submit = document.querySelector('.form')
+    if (submit) {
+        submit.addEventListener('submit', handleSubmit)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        const [firstNameInput, lastNameInput] = e.target.elements
+        if (firstNameInput.value && lastNameInput.value) {
+            addNewUser(firstNameInput.value, lastNameInput.value)
+        }
+    }
+}
+
+function addNewUser(firstName, lastName) {
+    fetchPost(apiUrl, { firstName, lastName })
+    .then(resp => {
+        if (resp.ok) {
+            return resp.json();
+        }
+
+        return Promise.reject(resp);
+    })
+    .catch(err => console.error(err))
+    .finally(() => loadUsers())
+}
+
+function fetchPost(url, data) {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+    })
 }
