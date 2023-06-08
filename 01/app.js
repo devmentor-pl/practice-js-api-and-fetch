c2bda8af82b2704878bb295274c213cff14f2ddb
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-08 20:14:42 +0200

    refactor code

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,31 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+  const divList = document.querySelectorAll("div");
 
+  // setBorderColorAsync(divList[0], 'red', function() {
+  //     setBorderColorAsync(divList[1], 'blue', function() {
+  //         setBorderColorAsync(divList[2], 'green', function() {
+  //             console.log('finish');
+  //         });
+  //     });
+  // });
+
+  setBorderColorAsync(divList[0], "red", firstCallBack);
+  function firstCallBack() {
+    setBorderColorAsync(divList[1], "blue", secondCallBack);
+  }
+  function secondCallBack() {
+    setBorderColorAsync(divList[2], "green", thirdCallBack);
+  }
+  function thirdCallBack() {
+    console.log("finish");
+  }
 }
 
 function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+  setTimeout(() => {
+    element.style.border = `3px solid ${color}`;
+    callback();
+  }, Math.random() * 3000);
 }
