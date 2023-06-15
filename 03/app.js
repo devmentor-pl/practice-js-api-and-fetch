f65d4c32222e99ec8b9ac2f284c6a2b55d4930a0
Jakub Kasperkowicz <jakub.kasperkowicz@gmail.com>
2023-06-15 23:24:57 +0200

    Finished 03

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,31 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    const btnEl = document.querySelector('button')
+    const spanEl = document.querySelector('span')
+    if(btnEl) {
+        btnEl.addEventListener(
+        'click',
+        getIp)
+    }
+
+    function getIp() {
+        const promise = fetch('https://api.ipify.org?format=json')
+    
+        promise.then(resp => {
+            if(resp.ok) {
+                return resp.json()
+            }   
+        }).then(data => {
+            if(spanEl) {
+                spanEl.innerText = data.ip
+                console.log(data)
+            } else {
+                console.error('Element not found')
+            }
+        })
+        .catch(err => console.log('I can not download data', err))
+    
+    }
 }
