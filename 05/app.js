7c267144782eda3ac1b6160b234de33d22b662b8
Michal Nowakowski <nowakowski.m1995@gmail.com>
2023-09-13 19:31:34 +0200

    ex.5 done

--- 05/app.js
+++ 05/app.js
@@ -1,37 +1,76 @@
-const apiUrl = 'http://localhost:3000/users';
+const apiUrl = "http://localhost:3000/users";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    loadUsers();
+  loadUsers();
+  addUser();
 }
 
 function loadUsers() {
-    const promise = fetchGet(apiUrl);
+  const promise = fetchGet(apiUrl);
 
-    promise
-        .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+  promise.then((data) => insertUsers(data)).catch((err) => console.error(err));
 }
 
 function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
+  return fetch(url).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    }
 
-            return Promise.reject(resp);
-        });
+    return Promise.reject(resp);
+  });
+}
+
+async function fetchPost(url, data) {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error("Error occured" + response.status);
+  }
+
+  return console.log(response);
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+  const ulElement = document.querySelector(".users");
+  ulElement.innerHTML = "";
+  usersList.forEach((user) => {
+    const liElement = document.createElement("li");
+    liElement.innerText = `${user.firstName} ${user.lastName}`;
 
-        ulElement.appendChild(liElement);
+    ulElement.appendChild(liElement);
+  });
+}
+
+function addUser() {
+  const form = document.querySelector(".form");
+  form.addEventListener("submit", handleSubmit);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const firstName = e.currentTarget.elements[0];
+  const lastName = e.currentTarget.elements[1];
+
+  if (firstName.value === "" || lastName.value === "")
+    return console.log("Pola nie moga byc puste");
+
+  const data = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+  };
+
+  fetchPost(apiUrl, data)
+    .catch((err) => console.error(err))
+    .finally(() => {
+      loadUsers();
+      firstName.value = "";
+      lastName.value = "";
     });
 }
