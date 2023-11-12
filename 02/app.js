369ce243b1f256064c43a91d6442c9682036d7b6
Kaczupinko <kaczupinko@gmail.com>
2023-11-12 20:10:11 +0000

    Finish 02 task

--- 02/app.js
+++ 02/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const divList = document.querySelectorAll('div');
-    
+    // Pytanie czy skoro setBorderColorAsync zwraca Promise to czy moge poprawic funkcje init wykorzystujac lancuchowanie promisow?
     setBorderColorAsync(divList[0], 'red', function() {
         setBorderColorAsync(divList[1], 'blue', function() {
             setBorderColorAsync(divList[2], 'green', function() {
@@ -14,19 +14,22 @@ function init() {
 }
 
 function setBorderColorAsync(element, color, callback) {
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        
-        if(callback && typeof callback === 'function') {
+    return new Promise((resolve, reject) => {
+        if(element && element instanceof HTMLElement) {
+            // sprawdzam czy parametr jest elementem DOM, więcej:
+            // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
             setTimeout(() => {
                 element.style.border = `3px solid ${color}`;
-                callback();
+                element.style.opacity = 1; // <-- edit: I add this line to make the div visible ;D
+                if(callback && typeof callback === 'function') {
+                    callback();
+                }
+                resolve();
             }, Math.random() * 3000);
         } else {
-            alert('Parametr ~callback~ mus być funkcją');
+            const errorMsg = 'Parametr ~element~ musi być prawidłowym elementem DOM';
+            alert(errorMsg);
+            reject(errorMsg);
         }
-    } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
-    }
+    });
 }
