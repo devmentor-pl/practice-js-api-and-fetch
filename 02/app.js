b36d27ed140ec988e8da78e1c2860648ba794a38
Kaczupinko <kaczupinko@gmail.com>
2023-11-15 11:12:12 +0000

    Change setBorderColorAsync  to chaining promises

--- 02/app.js
+++ 02/app.js
@@ -3,14 +3,11 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const divList = document.querySelectorAll('div');
     // Pytanie czy skoro setBorderColorAsync zwraca Promise to czy moge poprawic funkcje init wykorzystujac lancuchowanie promisow?
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
-
+    setBorderColorAsync(divList[0], 'red')
+        .then(() => setBorderColorAsync(divList[1], 'blue'))
+        .then(() => setBorderColorAsync(divList[2], 'green'))
+        .then(() => console.log('finish'))
+        .catch(error => console.error(error));
 }
 
 function setBorderColorAsync(element, color, callback) {
