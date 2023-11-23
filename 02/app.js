f19e8576dc390c51cf00764d97fe939911a47549
Piotr Marczykowski <piotr.marczykowski94@gmail.com>
2023-11-23 19:27:19 +0100

    add task 2

--- 02/app.js
+++ 02/app.js
@@ -1,32 +1,38 @@
-document.addEventListener('DOMContentLoaded', init);
-
-function init() {
+document.addEventListener('DOMContentLoaded', () => {
     const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
+
+    setBorderColorAsync(divList[0], 'red')
+        .then(() => setBorderColorAsync(divList[1], 'blue'))
+        .then(() => setBorderColorAsync(divList[2], 'green'))
+        .then(() => {
+            console.log('finish');
+        })
+        .catch((error) => {
+            console.error(error.message);
         });
-    });
+});
 
-}
+function setBorderColorAsync(element, color) {
+    
+return new Promise((resolve, reject) => {
 
-function setBorderColorAsync(element, color, callback) {
     if(element && element instanceof HTMLElement) {
         // sprawdzam czy parametr jest elementem DOM, więcej:
         // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
         
-        if(callback && typeof callback === 'function') {
+        if(color   && typeof color  === 'string') {
             setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
+                element.style.border = `3px solid ${color }`;
+                resolve();
             }, Math.random() * 3000);
         } else {
-            alert('Parametr ~callback~ mus być funkcją');
+            reject(new Error('Parametr ~color~ mus być stringiem'))
+            
         }
     } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
+        reject(new Error('Paremetr ~element~ musi być prawidłowym elementem DOM'))
     }
+})
+
+    
 }
