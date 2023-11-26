b11018ddea44d9cc17b642fcf8e1e6c7c720159f
Piotr Marczykowski <piotr.marczykowski94@gmail.com>
2023-11-26 16:29:31 +0100

    add task 5

--- 05/app.js
+++ 05/app.js
@@ -4,7 +4,48 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    addPerson();
 }
+
+const addPerson =() =>{
+    const formEl = document.querySelector('.form');
+    if(formEl) {
+        formEl.addEventListener('submit', handleSubmit);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const [firstName, lastName] = event.target.elements;
+        if(firstName.value && lastName.value) {
+            addNewUser(firstName.value, lastName.value);
+        }
+    }
+}
+
+const addNewUser = (firstName, lastName) => {
+    fetchPost(apiUrl, {firstName, lastName})
+        .then(resp => {
+            if(resp.ok) {
+                return resp.json();
+            }
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .finally(loadUsers)
+}
+
+const fetchPost = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
 
 function loadUsers() {
     const promise = fetchGet(apiUrl);
@@ -17,7 +58,7 @@ function loadUsers() {
 function fetchGet(url) {
     return fetch(url)
         .then(resp => {
-            if(resp.ok) {
+            if (resp.ok) {
                 return resp.json();
             }
 
