8951054fbe08c43d162d95d8444615f3854f9d49
Kasia <k.kasia912@gmail.com>
2023-12-27 10:10:29 +0100

    complete task 03

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,27 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
+  console.log('DOM');
+  const url = 'https://api.ipify.org?format=json';
+  const buttonEl = document.querySelector('button');
+  buttonEl && buttonEl.addEventListener('click', () => getIP(url));
+}
+
+async function getIP(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Błąd: ${response.status}`);
+    } else {
+      const data = await response.json();
+      showIP(data);
+      console.log(data.ip);
+    }
+  } catch (error) {
+    console.log('Błąd:', error);
+  }
+}
+function showIP(data) {
+  const spanEl = document.querySelector('span');
+  spanEl.textContent = data.ip;
 }
