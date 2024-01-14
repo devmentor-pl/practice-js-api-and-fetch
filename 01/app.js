3815f695f03b3a5dd6438bd4943604cb8b2f4612
Marcin Chechelski <marcin.chechelski@op.pl>
2024-01-14 11:37:45 +0100

    task 01 finish

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,30 @@
-//TEST//
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     const divList = document.querySelectorAll("div");
+    const colors = ["red", "blue", "green"];
 
-    setBorderColorAsync(divList[0], "red", function () {
-        setBorderColorAsync(divList[1], "blue", function () {
-            setBorderColorAsync(divList[2], "green", function () {
-                console.log("finish");
-            });
+    setBorderColorSequentially(Array.from(divList), colors)
+        .then(() => {
+            console.log("finish");
+        })
+        .catch((error) => {
+            console.error(error);
         });
+}
+function setBorderColorAsync(element, color) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            element.style.border = `3px solid ${color}`;
+            resolve();
+        }, Math.random() * 3000);
     });
 }
 
-function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+function setBorderColorSequentially(elements, colors) {
+    return elements.reduce((promise, element, index) => {
+        return promise.then(() => {
+            return setBorderColorAsync(element, colors[index]);
+        });
+    }, Promise.resolve());
 }
