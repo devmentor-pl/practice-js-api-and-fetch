885158768b37df9cfe67b949c6afc5e463c11a47
Marcin Chechelski <marcin.chechelski@op.pl>
2024-01-14 10:33:04 +0100

    test

--- 01/app.js
+++ 01/app.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', init);
+//TEST//
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
+    const divList = document.querySelectorAll("div");
+
+    setBorderColorAsync(divList[0], "red", function () {
+        setBorderColorAsync(divList[1], "blue", function () {
+            setBorderColorAsync(divList[2], "green", function () {
+                console.log("finish");
             });
         });
     });
-
 }
 
 function setBorderColorAsync(element, color, callback) {
