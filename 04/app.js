0ae2f060d32e4c3ba8e6936ac54c5b766346d787
Dawidtk <dawidtk@gmail.com>
2024-01-23 12:53:04 +0100

    weatherInfo

--- 04/app.js
+++ 04/app.js
@@ -16,16 +16,25 @@ function getWeather() {
 };
 
 function WeatherData(latitude, longitude) {
-    const api = 'https://api.weatherbit.io/v2.0/current?';
-    const apiKey = '00000 number';
+    const api = 'https://api.weatherbit.io/v2.0/current';
+    const apiKey = '2152939f82134cf69940f7dd9139bf89';
     return fetch(`${api}key=${apiKey}&lat=${latitude}&lon=${longitude}&lang=pl`)
         .then(response => {
             if (response.ok) {
                 return response.json()
-            } else throw new Error('Please try again. Can't get current weather')
+            } else throw new Error('Please try again. Cant get current weather')
         }).then(data => {
             displayWeatherInformation(data, latitude, longitude)
         });
+};
+
+function weatherInfo(fetchData, latitude, longitude) {
+    const [lat, lon, summary, temperature] = document.querySelectorAll('strong');
+    const { data: [{ temp, weather: { description } }] } = fetchData;
+    lat.textContent = latitude;
+    lon.textContent = longitude;
+    summary.textContent = description;
+    temperature.textContent = `${temp}°C`;
 };
 
 
