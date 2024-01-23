a9513d106d808d43e2136c8ff8a158a9ab9a0366
Dawidtk <dawidtk@gmail.com>
2024-01-23 09:00:41 +0100

    init

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
-}
+
+    const buttonEl = document.querySelector('button');
+    const spanEl = document.querySelector('span');
+
+
