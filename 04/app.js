b5c41ffc5d273831aef534a96894320be82bc0ac
Dawidtk <dawidtk@gmail.com>
2024-01-23 10:50:37 +0100

    init, getWeather

--- 04/app.js
+++ 04/app.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
-}
+    getWeather();
+};
+
+function getWeather() {
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const [lat, lon] = [...e.currentTarget.elements];
+        if (simplyValid(lat.value, lon.value)) {
+            weatherData(lat.value,lon.value)
+        };
+    });
+};
+
