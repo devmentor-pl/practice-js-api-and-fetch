15e873573c7a1f55d398570f692cf7876f2f6c13
Wojciech Flasiński <wojciech.flasinski@gmail.com>
2024-02-28 22:17:06 +0100

    ex.5 finish

--- 05/app.js
+++ 05/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    addUsers()
 }
 
 function loadUsers() {
@@ -15,23 +16,50 @@ function loadUsers() {
 }
 
 function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
-
-            return Promise.reject(resp);
-        });
+  return fetch(url)
+    .then(resp => {
+        if(resp.ok) {
+            return resp.json();
+        }
+        return Promise.reject(resp);
+    });
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+  const ulElement = document.querySelector('.users');
+  ulElement.innerHTML = '';
+  usersList.forEach(user => {
+    const liElement = document.createElement('li');
+    liElement.innerText = `${user.firstName} ${user.lastName}`;
 
-        ulElement.appendChild(liElement);
-    });
+    ulElement.appendChild(liElement);
+  });
+}
+
+function addUsers () {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const [first, last] = e.target.elements
+    // nie wiem dlaczego nie zadziałało z {} jak w przykładzie
+    // const { first, last } = e.target.elements 
+    
+    const data = {
+      firstName: first.value, lastName: last.value
+    }
+    console.log(data)
+    
+    const options = {
+      method: 'POST',
+      body: JSON.stringify( data ),
+      headers: { 'Content-Type': 'application/json'}
+    }
+
+    fetch(apiUrl, options)
+      .then(resp => console.log(resp))
+      .catch(err => console.error(err))
+      .finally( loadUsers )
+
+  })
 }
