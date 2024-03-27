4b4081baf37a76d3058d035a132e480f4891fbef
Typee8 <wojciechjasiak1324@gmail.com>
2024-03-27 16:49:50 +0100

    Finish 03

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,25 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log('DOM');
+  console.log("DOM");
+  
+  const button = document.querySelector("button");
+  const span = document.querySelector("span");
+
+  fetchIp().then((resp) => {
+    button.addEventListener("click", () => showIp(resp, span));
+  });
+}
+
+function showIp(data, place) {
+  console.log(place);
+  place.innerText = data.ip;
+}
+
+function fetchIp() {
+  return fetch("https://api.ipify.org?format=json").then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    }
+  });
 }
