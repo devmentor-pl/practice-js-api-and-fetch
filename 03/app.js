b67417718b380131756cc94555a4a8ff5ffa4e48
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-04-04 08:55:40 +0200

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -3,3 +3,12 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     console.log('DOM');
 }
+
+const promise = fetch('https://api.ipify.org?format=json');
+
+const spanEl = document.querySelector('span');
+console.log(spanEl);
+
+const ip = 
+promise
+.then(spanEl.innerText = ip); // BŁĄD
