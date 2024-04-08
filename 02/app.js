58b740cd473fa31eca034097d6878cd384004ab9
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-04-08 14:42:33 +0200

    task 02 changed acc to PR

--- 02/app.js
+++ 02/app.js
@@ -5,10 +5,11 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const divList = document.querySelectorAll('div');
     
-    setBorderColorAsync(divList[0], 'red').then(() =>
-        setBorderColorAsync(divList[1], 'blue')).then(() => 
-            setBorderColorAsync(divList[2], 'green')).then(() =>
-                console.log('finish')).catch(error => console.error(error));
+    setBorderColorAsync(divList[0], 'red')
+        .then(() => setBorderColorAsync(divList[1], 'blue'))
+        .then(() => setBorderColorAsync(divList[2], 'green'))
+        .then(() => console.log('finish'))
+        .catch(error => console.error(error));
 }
 
 function setBorderColorAsync(element, color) {
