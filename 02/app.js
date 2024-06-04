e65dc8e500e88820f841c8bfa7a33c015ee56ab5
Jaro <jaroswidnik@interia.pl>
2024-06-04 10:36:11 +0200

    Fisnihed ex2 in practice of API and fetch

--- 02/app.js
+++ 02/app.js
@@ -3,34 +3,26 @@ document.addEventListener("DOMContentLoaded", init)
 function init() {
 	const divList = document.querySelectorAll("div")
 
-	setBorderColorAsync(divList[0], "red", function () {
-		setBorderColorAsync(divList[1], "blue", function () {
-			setBorderColorAsync(divList[2], "green", function () {
-				console.log("finish")
-			})
+	// setBorderColorAsync(divList[0], "red", function () {
+	// 	setBorderColorAsync(divList[1], "blue", function () {
+	// 		setBorderColorAsync(divList[2], "green", function () {
+	// 			console.log("finish")
+	// 		})
+	// 	})
+	// })
+
+
+	setBorderColorAsync(divList[0], 'red')
+		.then(() => setBorderColorAsync(divList[1], 'blue'))
+		.then(() => setBorderColorAsync(divList[2]), 'green')
+		.finally(() => {
+			console.log('Finish ??');
 		})
-	})
 }
 
-function setBorderColorAsync(element, color, callback) {
-	if (element && element instanceof HTMLElement) {
-		// sprawdzam czy parametr jest elementem DOM, więcej:
-		// https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
 
-		if (callback && typeof callback === "function") {
-			setTimeout(() => {
-				element.style.border = `3px solid ${color}`
-				callback()
-			}, Math.random() * 3000)
-		} else {
-			alert("Parametr ~callback~ mus być funkcją")
-		}
-	} else {
-		alert("Paremetr ~element~ musi być prawidłowym elementem DOM")
-	}
-}
 
-function promiseSetBorderColor(element, color) {
+function setBorderColorAsync(element, color) {
 	const promise = new Promise((resolve, reject) => {
 		if (element && element instanceof HTMLElement) {
 			setTimeout(() => {
@@ -41,4 +33,27 @@ function promiseSetBorderColor(element, color) {
 			reject("Paremetr ~element~ musi być prawidłowym elementem DOM")
 		}
 	})
+
+	return promise 
 }
+
+
+
+
+// function setBorderColorAsync(element, color, callback) {
+// 	if (element && element instanceof HTMLElement) {
+// 		// sprawdzam czy parametr jest elementem DOM, więcej:
+// 		// https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+
+// 		if (callback && typeof callback === "function") {
+// 			setTimeout(() => {
+// 				element.style.border = `3px solid ${color}`
+// 				callback()
+// 			}, Math.random() * 3000)
+// 		} else {
+// 			alert("Parametr ~callback~ mus być funkcją")
+// 		}
+// 	} else {
+// 		alert("Paremetr ~element~ musi być prawidłowym elementem DOM")
+// 	}
+// }
