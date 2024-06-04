0b772a27054f5da75c8b97b5548ce04695195cc6
Jaro <jaroswidnik@interia.pl>
2024-06-04 12:26:01 +0200

    Added selectors

--- 04/app.js
+++ 04/app.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    const formEl = document.querySelector('.form')
+    const latInput = document.querySelector('.form__field--lat')
+    const longtInput = document.querySelector('.form__field--lng')
+    const submitBtn = document.querySelector('.form__submit')
+
+    const latArea = document.querySelector('.weather__lat')
+    const longArea = document.querySelector('.weather__lng')
+    const weatherType = document.querySelector('.weather__summary')
+    const weatherTemp = document.querySelector('.weather__temperature')
+
 }
