eaebba083f92e4d202bc27c95acba326f1723c7b
Emilia Szafarska <emilia.anna.szafarska@gmail.com>
2024-06-08 15:49:40 +0200

    Add solution to task 05

--- 05/app.js
+++ 05/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    setupForm();
 }
 
 function loadUsers() {
@@ -34,4 +35,49 @@ function insertUsers(usersList) {
 
         ulElement.appendChild(liElement);
     });
+}
+
+function setupForm() {
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const firstNameInput = document.querySelector('.form__field--first-name');
+    const lastNameInput = document.querySelector('.form__field--last-name');
+
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
+
+    if(!firstName || !lastName) {
+        alert('Wpisz zarówno imię jak i nazwisko.');
+        return;
+    }
+
+    const newUser = {
+        firstName: firstName,
+        lastName: lastName
+    };
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        });
+        if (!response.ok) {
+            throw new Error('Network response failed');
+        }
+        firstNameInput.value = '';
+        lastNameInput.value = '';
+
+        loadUsers();
+    } catch (error) {
+        console.error(error);
+        alert('Nie udało się załadować nowego użytkownika.');
+    }
 }
