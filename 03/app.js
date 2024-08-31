9e4801e1964cecbeba2eb83ba18704c11f8b4148
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-08-31 16:55:01 +0200

    Add task 03

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,25 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log('DOM');
+  console.log("DOM");
+  const button = document.querySelector("button");
+  if (button) {
+    button.addEventListener("click", () => {
+      fetch("https://api.ipify.org?format=json")
+        .then((resp) => resp.json())
+        .then((ipContainer) => getIp(ipContainer))
+        .then((ip) => showIp(ip))
+        .catch((error) => console.log("Błąd: ", error));
+    });
+  }
 }
+
+const getIp = (ipContainer) => {
+  const ip = ipContainer.ip;
+  return ip;
+};
+
+const showIp = (ip) => {
+  const span = document.querySelector("span");
+  span.textContent = ip;
+};
