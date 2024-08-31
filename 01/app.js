bcd2bca70d545b63632b78b228e6fc128062a076
Marcin Wabinski <wabinskim@gmail.com>
2024-08-31 02:03:59 +0200

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,31 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
-
+  const divList = document.querySelectorAll("div");
+  firstCallback(divList);
 }
 
 function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+  setTimeout(() => {
+    element.style.border = `3px solid ${color}`;
+    callback();
+  }, Math.random() * 3000);
+}
+
+function firstCallback(divList) {
+  setBorderColorAsync(divList[0], "red", function () {
+    secondCallback(divList);
+  });
+}
+
+function secondCallback(divList) {
+  setBorderColorAsync(divList[1], "blue", function () {
+    thirdCallback(divList);
+  });
+}
+
+function thirdCallback(divList) {
+  setBorderColorAsync(divList[2], "green", function () {
+    console.log("finish");
+  });
 }
