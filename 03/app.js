09b401b724778b14a4bc45f8293df182e7afb82e
Marcin Wabinski <wabinskim@gmail.com>
2024-08-31 02:07:41 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,31 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log('DOM');
+  console.log("DOM");
+  showIp();
+}
+
+function getIp() {
+  return fetch("https://api.ipify.org?format=json")
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      }
+      Promise.reject(resp);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => console.log("Finish"));
+}
+
+function showIp() {
+  const button = document.querySelector("button");
+  const spanEl = document.querySelector("span");
+
+  button.addEventListener("click", () => {
+    getIp().then((data) => {
+      spanEl.innerText = data.ip;
+    });
+  });
 }
