062952643db92511586a8bf0e83716714269ef1c
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-17 17:42:01 +0200

    added solution to three task

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,23 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+    const button = document.querySelector("button");
+    const span = document.querySelector("span");
+    button.addEventListener("click", async () => {
+        const ip = await getIP()
+        span.innerHTML = ip.ip;
+    })
+}
+
+const getIP = async () => {
+    try {
+        const response = await fetch("https://api.ipify.org?format=json");
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const ip = await response.json();
+        return ip;
+    } catch (error) {
+        console.error(error)
+    }
 }
