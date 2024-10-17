831700c963bfaf9685631cd0d23e1c57d9afb322
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-17 16:39:40 +0200

    added solution to first task

--- 01/app.js
+++ 01/app.js
@@ -2,20 +2,19 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
 
-}
+    setBorderColorAsync(divList[0], "red")
+        .then(() => setBorderColorAsync(divList[1], "blue"))
+        .then(() => setBorderColorAsync(divList[2], "green"))
+        .then(() => console.log("finish"))
+};
 
-function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
-}
+function setBorderColorAsync(element, color) {
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            element.style.border = `3px solid ${color}`;
+            resolve()
+        }, Math.random() * 3000)
+    })
+    return promise
+};
