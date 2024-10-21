8a9ef224c134620da0d24a7b7d7a1e120bbabcd7
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-21 15:53:33 +0200

    formatted code

--- 03/app.js
+++ 03/app.js
@@ -12,6 +12,7 @@ function init() {
 
 const getIP = async () => {
     try {
+        
         const response = await fetch("https://api.ipify.org?format=json");
         if (!response.ok) {
             throw new Error(response.statusText);
