96135e6703d5b1d65c365063ff0246f2e1de9be8
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-22 16:20:27 +0200

    solution added to the fifth task

--- 05/app.js
+++ 05/app.js
@@ -3,7 +3,7 @@ const apiUrl = 'http://localhost:3000/users';
 document.addEventListener('DOMContentLoaded', init);
 
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (eve) => {
     event.preventDefault();
     const firstName = document.querySelector(".form__field--first-name").value;
     const lastName = document.querySelector(".form__field--last-name").value;
@@ -63,3 +63,18 @@ function insertUsers(usersList) {
         ulElement.appendChild(liElement);
     });
 }
+
+// W tym zadaniu będziesz potrzebować narzędzia [JSON Server](https://github.com/typicode/json-server), które uruchomi lokalne API na podstawie pliku `data.json`. JSON Server instalowaliśmy globalnie podczas przerabiania materiałów z tego modułu.
+
+// W pliku tym znajdują się już dane, które zostaną załadowane do HTML-a za pomocą kodu napisanego w `app.js` zaraz po uruchomieniu lokalnego API.
+
+// JSON Server można uruchomić w terminalu przy pomocy komendy: `json-server ./data.json --watch`.
+
+// Twoim zadaniem jest napisanie obsługi formularza, tak aby można było dodawać nowe dane do naszego lokalnego API.
+
+// Przypominam, że należy:
+// * wykorzystać odpowiednią metodę (`POST`),
+// * na podstawie wysłanych przez formularz danych utworzyć obiekt, który trzeba zamienić na format JSON (`JSON.stringify()`),
+// * przekazanć odpowiedni nagłówek (`'Content-Type': 'application/json'`).
+
+// Po każdorazowym dodaniu użytkownika zaktualizuj widok przy pomocy funkcji `loadUsers()`. Trzeba ją uruchomić w odpowiednim momencie, np. w `finally()`.
