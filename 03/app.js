310e4dfc5294278fb4d01ad14fc7351a37bf4b81
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 00:39:42 +0100

    task 3

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    document.querySelector('button').addEventListener('click', getIp);
+
+    function getIp() {
+        fetch('https://api.ipify.org?format=json')
+            .then((response) => response.json())
+            .then((data) => {
+                document.querySelector('span').innerText = data.ip;
+            })
+            .catch((err) => {
+                console.error('Błąd podczas pobierania adresu IP:', err);
+            });
+    }
 }
