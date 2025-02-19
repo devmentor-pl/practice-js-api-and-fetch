1e49b6b175cddfa0762c5521a1a9dd9ab75d0dd7
marrcelp <marcel.piaszczyk@gmail.com>
2025-02-19 17:03:26 +0100

    resolved exercise 1

--- 01/app.js
+++ 01/app.js
@@ -3,19 +3,26 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const divList = document.querySelectorAll('div');
     
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+    setBorderColorAsync(divList[0], 'red')
+        .then (() => setBorderColorAsync(divList[1], 'blue'))
+        .then (() => setBorderColorAsync(divList[2], 'green'))
+        .catch((err) => console.error(err))
+        .finally(() => console.log('zakonczono'));
 
 }
 
-function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+function setBorderColorAsync(element, color) {
+
+    return new Promise ((resolve, reject) => {
+
+        setTimeout(() => {
+            if(element){
+                element.style.border = `3px solid ${color}`;
+                resolve();
+            } else {
+                reject('Nie znaleziono elementu!');
+            }
+        }, Math.random() * 3000);
+    })
 }
+
