67a8d117e3543ddcb57713dbbacbe693846204c7
marrcelp <marcel.piaszczyk@gmail.com>
2025-02-21 15:41:13 +0100

    resolved exercise 3

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', init);
+const btn = document.querySelector('button');
+btn.addEventListener('click', showIp);
 
 function init() {
     console.log('DOM');
 }
+
+function showIp(){
+
+    const promise = fetch('https://api.ipify.org?format=json');
+
+    promise
+    .then(resp => {
+        if (resp.ok){
+            return resp.json();
+        }
+
+        return Promise.reject(resp);
+    })
+    .then(data => {
+        // console.log(data.ip);
+        const yourIp = document.querySelector('span');
+        yourIp.textContent = data.ip;
+    })
+    .catch(err => console.error(err))
+    .finally(() => console.log('Zakonczono odpytywanie API'));
+}
+
+
+
