fca5e52ae67cb50069216f3336f001d38e3f03ca
Marek Bełz <belzmarek@gmail.com>
2025-03-14 14:36:34 +0000

    task done

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
+    const button = document.querySelector('button');
+    const span = document.querySelector('span');
+
+    button.addEventListener('click', function() {
+        fetch('https://cors-anywhere.herokuapp.com/http://ip-api.com/json')
+            .then(resp => {
+                return resp.json();
+            })
+            .then(data => {
+                span.textContent = data.query;
+            })
+            .catch(error => {
+                console.log('Problem with operation: ', error);
+            });
+    });
 }
