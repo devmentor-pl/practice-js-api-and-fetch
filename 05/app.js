06fa5a5ce91fc9b1ceb1b287d21c633b0eb5acd9
Michał <m.majewski.dm@gmail.com>
2025-05-06 08:11:44 +0200

    task 05 done

--- 05/app.js
+++ 05/app.js
@@ -1,9 +1,20 @@
 const apiUrl = 'http://localhost:3000/users';
 
+let userForm = null
+let firstNameInput = null
+let lastNameInput = null
+let usersListElement = null
+
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+    userForm = document.querySelector('.form')
+    firstNameInput = document.querySelector('.form__field--first-name')
+    lastNameInput = document.querySelector('.form__field--last-name')
+    usersListElement = document.querySelector('.users')
+
     loadUsers();
+    setupFormListener();
 }
 
 function loadUsers() {
@@ -25,13 +36,70 @@ function fetchGet(url) {
         });
 }
 
+function fetchPost(url, data) {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(resp => {
+        if (resp.ok) {
+            return resp.json()
+        }
+        throw new Error(`error status: ${resp.status}`)
+    })
+}
+
 function insertUsers(usersList) {
     const ulElement = document.querySelector('.users');
     ulElement.innerHTML = '';
+    if (!usersListElement) {
+        console.log('element nie znaleziony');
+        return
+    }
+    usersListElement.innerHTML = ''
     usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+        if (!user || typeof user.firstName === 'undefined' || typeof user.lastName === 'undefined') {
+            return
+        }
+
+        const liElement = document.createElement('li')
+        liElement.innerText = `${user.firstName} ${user.lastName}`
 
         ulElement.appendChild(liElement);
-    });
+    })
+}
+
+
+function setupFormListener() {
+    if (!userForm || !firstNameInput || !lastNameInput) {
+        console.log('formularz nie znbaleziony');
+        return
+    }
+
+    userForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        
+    const newUser = {
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+    }
+
+    const promise = fetchPost(apiUrl, newUser)
+
+    promise
+        .then(addedUser => {
+            console.log('User dodany:', addedUser)
+        })
+        .catch(err => {
+            console.error('błąd dodawania user:', err);
+        })
+        .finally(() => {
+            loadUsers()
+            userForm.reset()
+        })
+    })
 }
