da7c5c96eae7415baf2e33f106a38a114b7cc749
Daria Seńko <twoj@email>
2025-08-13 20:29:33 +0200

    api

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,23 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
+  console.log('DOM');
+
+  const button = document.querySelector('button');
+  const span = document.querySelector('span');
+
+  button.addEventListener('click', fetchIP);
+
+  async function fetchIP() {
+    span.textContent = 'Pobieram';
+    try {
+      const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+
+      span.textContent = data.ip;
+    } catch (err) {
+      console.error(err);
+      span.textContent = 'Błąd pobierania';
+    }
+  }
 }
